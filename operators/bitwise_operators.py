9f2cb1941160d271b860c4326a6f240d72cb3bf1
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-11 18:06:28 +0530

    [ADD] bitwise operators examples

---
+++ operators/bitwise_operators.py
@@ -0,0 +1,11 @@
+"""Bitwise operators"""
+
+a = 5
+b = 3
+
+print("AND: ", (a & b))
+print("OR:  ", (a | b))
+print("XOR: ", (a ^ b))
+print("Not ~:", (~a))
+print("LEFT SHIFT: ",(a<<1))
+print("RIGHT SHIFT: ",(a>>1))
