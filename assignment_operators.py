71e43c10583b9f2f38c56e8dbdaae5e7210effb4
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-11 16:04:30 +0530

    [ADD] assignment operators examples

---
+++ assignment_operators.py
@@ -0,0 +1,11 @@
+"""Assignment operators"""
+
+x = 5
+
+x += 5      # same as x = x + 5, Result = 10
+x -= 2      # same as x = x - 2, Result = 8
+x *= 2      # same as x = x * 2, Result = 16
+x /= 2      # same as x = x / 2, Result = 8.0
+x //= 3     # same as x = x // 3, Result = 2.0
+x %= 2      # same as x = x % 2, Result = 0.0
+x **= 1     # same as x = x ** 2, Result = 0.0
