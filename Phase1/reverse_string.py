ef53450869678bf826df383f5f0a392b98a71f29
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-13 21:04:12 +0530

    [IMP] Improve reversal string using .join() and reversed()

--- Phase1/reverse_string.py
+++ Phase1/reverse_string.py
@@ -2,9 +2,6 @@
 
 text = input("Enter a text: ")
 
-reversed_string = ""
-
-for index in range(len(text) - 1, -1, -1):
-    reversed_string += text[index]
+reversed_string = "".join(reversed(text))
 
 print(f"Reversed string: {reversed_string}")
