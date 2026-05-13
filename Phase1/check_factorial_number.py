d5caa14d946c48bcfef6102461ffbeefe27009fe
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-13 15:56:39 +0530

    [IMP] Improve factorial calculator using while loop

--- Phase1/check_factorial_number.py
+++ Phase1/check_factorial_number.py
@@ -3,13 +3,15 @@
 try:
     n = int(input("Enter a number: "))
     if n < 0:
-        print("Factorial number is not defined for negative numbers.")
+        print("Factorial is not defined for negative numbers.")
     elif n == 0:
         print(f"Factorial number {n} is 1.")
     else:
         fact = 1
-        for i in range(1, n + 1):
+        i = 1
+        while i <= n:
             fact *= i
+            i += 1
         print(f"Factorial of {n} is {fact}")
 except ValueError:
     print("Invalid input! Please enter an integer.")
