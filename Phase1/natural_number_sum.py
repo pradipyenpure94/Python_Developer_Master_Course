ef8e86edcb3187baff0c33ed8cb64f4bcebf0366
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-13 14:35:24 +0530

    [IMP] Improve while loop natural numbers sum validation

--- Phase1/natural_number_sum.py
+++ Phase1/natural_number_sum.py
@@ -1,4 +1,4 @@
-"""Sum of natural numbers."""
+"""Calculating the sum of natural numbers."""
 
 try:
     n = int(input("Enter a number: "))
@@ -6,8 +6,10 @@ try:
         print("Negative number is not defined for calculating sum of natural numbers.")
     else:
         total = 0
-        for number in range(1, n + 1):
-            total += number
+        i = 1
+        while i <= n:
+            total += i
+            i += 1
         print(f"The sum of natural numbers from 1 to {n} is {total}")
 except ValueError:
     print("Invalid input! Please enter an integer.")
