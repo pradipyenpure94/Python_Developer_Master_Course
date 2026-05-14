72d36c43b3ee952280d9b945ada3941828ae0669
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-14 12:36:41 +0530

    [ADD] Add right triangle number patter

--- Phase1/right_triangle_pattern.py
+++ Phase1/right_triangle_pattern.py
@@ -1,6 +1,6 @@
-"""Right triangle star pattern."""
+"""Right triangle number pattern."""
 
-i = 0
-while i < 10:
-    print(" * "*i)
-    i += 1
+for i in range(1, 10):
+    for _ in range(1, i + 1):
+        print(i, end=" ")
+    print()
