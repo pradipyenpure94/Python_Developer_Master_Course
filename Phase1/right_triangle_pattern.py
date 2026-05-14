59438aee574eaedd01d52b7b459e907b32b27272
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-14 12:40:53 +0530

    [ADD] Add increasing number triangle pattern

--- Phase1/right_triangle_pattern.py
+++ Phase1/right_triangle_pattern.py
@@ -1,6 +1,6 @@
 """Right triangle number pattern."""
 
 for i in range(1, 10):
-    for _ in range(1, i + 1):
-        print(i, end=" ")
+    for j in range(1, i + 1):
+        print(j, end=" ")
     print()
