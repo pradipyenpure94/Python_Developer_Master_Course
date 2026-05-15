7d14e83b1bf5aea859bdabfefd0830c575534e01
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-15 15:24:34 +0530

    [IMP] Implement manual smallest number finder using while loop.

--- list/find_smallest_number.py
+++ list/find_smallest_number.py
@@ -4,10 +4,13 @@ numbers = [10, 50, 20, 90, 40]
 
 if numbers:
     smallest_number = numbers[0]
+    i = 1
+    length = len(numbers)
 
-    for number in numbers[1:]:
-        if number < smallest_number:
-            smallest_number = number
+    while i < length:
+        if numbers[i] < smallest_number:
+            smallest_number = numbers[i]
+        i += 1
 
     print(f"Smallest number: {smallest_number}")
 else:
