57a379f5a822136dd726efcbe3ab17dd5cd4fd72
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-15 16:17:51 +0530

    [IMP] Implement even number filter using filter() and lambda.

--- list/even_numbers_from_list.py
+++ list/even_numbers_from_list.py
@@ -1,6 +1,5 @@
 """Even numbers from list."""
 
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-
-even_numbers = [num for num in numbers if num % 2 == 0]
+even_numbers = list(filter(lambda num: num % 2 == 0, numbers))
 print(f"Even numbers: {even_numbers}")
