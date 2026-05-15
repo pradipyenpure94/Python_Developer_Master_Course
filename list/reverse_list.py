40d9fe6bbfb34bc898ca5d10b821315ae56fcb51
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-15 18:18:59 +0530

    [IMP] Implement manual list reversal using while loop

--- list/reverse_list.py
+++ list/reverse_list.py
@@ -2,10 +2,11 @@
 
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-length = len(numbers)
+index = len(numbers) - 1
 reversed_list = []
 
-for index in range(length - 1, -1, -1):
+while index >= 0:
     reversed_list.append(numbers[index])
+    index -= 1
 
 print(f"Reversed List: {reversed_list}")
