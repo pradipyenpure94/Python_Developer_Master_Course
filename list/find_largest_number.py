a8c9c974e93ac8f5c34110381856a6e84284bff2
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-15 14:46:07 +0530

    [IMP] Add custom logic to find largest number without max()

--- list/find_largest_number.py
+++ list/find_largest_number.py
@@ -4,7 +4,12 @@
 numbers = [10, 50, 20, 90, 40]
 
 if numbers:
-    largest_number = max(numbers)
+    largest_number = numbers[0]
+
+    for number in numbers[1:]:
+        if number > largest_number:
+            largest_number = number
+
     print(f"Largest number: {largest_number}")
 else:
     print("List is empty!")
