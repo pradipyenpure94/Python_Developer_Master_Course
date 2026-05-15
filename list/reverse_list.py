06b18ef0c41d14e09d5ab275e0757d20fe592e43
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-15 18:11:37 +0530

    [IMP] Add iterative logic for reversing list elements.

--- list/reverse_list.py
+++ list/reverse_list.py
@@ -1,5 +1,11 @@
 """Reverse List"""
 
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-numbers.reverse()
-print(f"Reversed List: {numbers}")
+
+length = len(numbers)
+reversed_list = []
+
+for index in range(length - 1, -1, -1):
+    reversed_list.append(numbers[index])
+
+print(f"Reversed List: {reversed_list}")
