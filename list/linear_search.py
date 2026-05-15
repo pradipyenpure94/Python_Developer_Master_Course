ef0eff56e9164ead079f2dd4ba67edb95b26e03a
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-15 21:47:55 +0530

    [IMP] Implement linear search using while loop.

--- list/linear_search.py
+++ list/linear_search.py
@@ -1,11 +1,15 @@
 """Linear search."""
 
 numbers = [10, 20, 30, 40, 50, 60, 70]
-search_number = 40
 
-for index, number in enumerate(numbers):
-    if search_number == number:
+search_number = 40
+index = 0
+length = len(numbers)
+
+while index < length:
+    if search_number == numbers[index]:
         print(f"Number found at index {index}")
         break
+    index += 1
 else:
     print("Number not found in list.")
