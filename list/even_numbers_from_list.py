dba99b3240dd5009c9bee5b3c2e4da1c0d77300f
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-15 16:11:53 +0530

    [IMP] Optimize even number extraction with list comprehension

--- list/even_numbers_from_list.py
+++ list/even_numbers_from_list.py
@@ -2,10 +2,5 @@
 
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-i = 0
-length = len(numbers)
-
-while i < length:
-    if numbers[i] % 2 == 0:
-        print(numbers[i])
-    i += 1
+even_numbers = [num for num in numbers if num % 2 == 0]
+print(f"Even numbers: {even_numbers}")
