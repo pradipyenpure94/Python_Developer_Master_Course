d0b3e7b0501f71b9a402ccb30039a3b31d34661b
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-15 15:13:22 +0530

    [IMP] Implement smallest number search logic using for loop

--- list/find_smallest_number.py
+++ list/find_smallest_number.py
@@ -3,7 +3,12 @@
 numbers = [10, 50, 20, 90, 40]
 
 if numbers:
-    smallest_number = min(numbers)
+    smallest_number = numbers[0]
+
+    for number in numbers[1:]:
+        if number < smallest_number:
+            smallest_number = number
+
     print(f"Smallest number: {smallest_number}")
 else:
     print("List is empty!")
