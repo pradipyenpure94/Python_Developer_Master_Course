47768ad7833ca3b4a268ad25f29902f6e07ec90a
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-15 15:54:09 +0530

    [IMP] Implement the sum of list elements using while loop.

--- list/sum_of_list.py
+++ list/sum_of_list.py
@@ -3,8 +3,11 @@
 numbers = [1, 2, 3, 4, 5, 6]
 
 total = 0
+length = len(numbers)
+i = 0
 
-for number in numbers:
-    total += number
+while i < length:
+    total += numbers[i]
+    i += 1
 
 print(f"Sum of list elements: {total}")
