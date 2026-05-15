496a34d0359262448558fc29bf4ee0db2afc5304
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-15 15:03:19 +0530

    [ADD] Implement smallest number finder using max()

---
+++ list/find_smallest_number.py
@@ -0,0 +1,9 @@
+"""Find the smallest number."""
+
+numbers = [10, 50, 20, 90, 40]
+
+if numbers:
+    smallest_number = min(numbers)
+    print(f"Smallest number: {smallest_number}")
+else:
+    print("List is empty!")
