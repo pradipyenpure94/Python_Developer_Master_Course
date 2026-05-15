6bde014f16223dcb0c58bec9019c42cf56e92c24
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-15 16:59:06 +0530

    [IMP] Improve duplicate removal using while loop.

--- list/remove_duplicates_item.py
+++ list/remove_duplicates_item.py
@@ -3,9 +3,14 @@
 colors = ["red", "green", "yellow", "blue", "green", "red"]
 
 unique_colors = []
+index = 0
+length = len(colors)
 
-for color in colors:
-    if color not in unique_colors:
-        unique_colors.append(color)
+while index < length:
+    current_color = colors[index]
+
+    if current_color not in unique_colors:
+        unique_colors.append(current_color)
+    index += 1
 
 print(f"Unique colors: {unique_colors}")
