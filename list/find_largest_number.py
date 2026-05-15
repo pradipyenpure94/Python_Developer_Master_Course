2d6e10835ce19bb53363ac81d63dd71b4bd3cd74
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-15 14:56:57 +0530

    [IMP] Improve custom logic to find largest number using while loop.

--- list/find_largest_number.py
+++ list/find_largest_number.py
@@ -5,10 +5,13 @@ numbers = [10, 50, 20, 90, 40]
 
 if numbers:
     largest_number = numbers[0]
+    i = 1
+    length = len(numbers)
 
-    for number in numbers[1:]:
-        if number > largest_number:
-            largest_number = number
+    while i < length:
+        if numbers[i] > largest_number:
+            largest_number = numbers[i]
+        i += 1
 
     print(f"Largest number: {largest_number}")
 else:
