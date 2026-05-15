fa3b5ec0989c1e096582a5616dfbbc2dc1f6f42e
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-15 14:21:23 +0530

    [ADD] Add to find the largest number in list

---
+++ list/find_largest_number.py
@@ -0,0 +1,10 @@
+"""Find the largest number."""
+
+
+numbers = [10, 50, 20, 90, 40]
+
+if numbers:
+    largest_number = max(numbers)
+    print(f"Largest number: {largest_number}")
+else:
+    print("List is empty!")
