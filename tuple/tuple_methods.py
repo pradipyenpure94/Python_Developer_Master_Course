9b098a31a9e5a5481775b3e8a75163bb876cefa0
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-16 20:23:52 +0530

    [ADD] Tuple count() and index() methods

---
+++ tuple/tuple_methods.py
@@ -0,0 +1,9 @@
+"""Tuple methods"""
+
+# count(value)
+t = (1, 2, 3, 4, 2, 1)
+print(f"Number count: {t.count(2)}")
+
+# index(value, start, stop)
+t = (1, 2, 3, 4, 2, 1)
+print(f"Index: {t.index(2, 3, 6)}")
