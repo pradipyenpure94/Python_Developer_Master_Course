8c128d756911c36a7432b8b66901a99574e13b5d
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-16 13:37:59 +0530

    [IMP] Implement right rotation using list slicing

--- list/list_rotation.py
+++ list/list_rotation.py
@@ -1,7 +1,6 @@
-"""List rotation by left"""
+"""List rotation by right"""
 
 numbers = [1, 2, 3, 4, 5]
-
 k = 2
-left_shift = numbers[k:] + numbers[:k]
-print(f"Left shift by {k}: {left_shift}")
+right_shift = numbers[-k:] + numbers[:-k]
+print(f"Right shift by {k}: {right_shift}")
