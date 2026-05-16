88e1331f89be65b4ca0c3cc1e4f64660402a0c4c
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-16 16:34:32 +0530

    [IMP] Find common elements between two list using filter()

--- list/find_common_elements.py
+++ list/find_common_elements.py
@@ -2,5 +2,5 @@
 
 prime_numbers = [2, 3, 5, 7, 11, 13]
 odd_numbers = [1, 3, 5, 7, 9, 11, 13]
-common_numbers = [num for num in prime_numbers if num in odd_numbers]
+common_numbers = list(filter(lambda x: x in odd_numbers, prime_numbers))
 print(f"Common elements: {common_numbers}")
