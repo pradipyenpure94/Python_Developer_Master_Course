ca2fa62fdc0c25f47a1df6fccd815b67f23b5cba
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-16 16:07:52 +0530

    [IMP] Filter odd numbers using filter()

--- list/find_odd_numbers.py
+++ list/find_odd_numbers.py
@@ -1,5 +1,5 @@
 """Find the odd numbers."""
 
 numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-odd_numbers = [num for num in numbers if num % 2 == 1]
+odd_numbers = list(filter(lambda x: x % 2 == 1, numbers))
 print(f"Odd numbers: {odd_numbers}")
