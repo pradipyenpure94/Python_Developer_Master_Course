c5604bfdfb80480ffe88e2b4a9a8ee1aebba988e
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-16 12:25:05 +0530

    [IMP] Implement matrix addition using nested while loop.

--- list/matrix_addition.py
+++ list/matrix_addition.py
@@ -4,10 +4,17 @@ list1 = [[1, 2, 3], [4, 5, 6]]
 list2 = [[4, 5, 3], [9, 5, 16]]
 
 result = []
-for i, _ in enumerate(list1):
+i = 0
+
+while i < len(list1):
     row = []
-    for j, _ in enumerate(list1[0]):
+    j = 0
+
+    while j < len(list1[0]):
         row.append((list1[i][j] + list2[i][j]))
+        j += 1
+    i += 1
+
     result.append(row)
 
 print(f"Matrix addition: {result}")
