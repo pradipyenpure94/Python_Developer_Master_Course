80b36ab95be9a517ffe4a44ef92c398fbcd3f2a5
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-16 15:00:14 +0530

    [IMP] Separate positive and negative numbers using filter

--- list/filter_numbers.py
+++ list/filter_numbers.py
@@ -2,18 +2,8 @@
 
 numbers = [0, 1, 2, -3, 4, -5, 6, -1, 2, 3]
 
-positive_numbers = []
-negative_numbers = []
-index = 0
-length = len(numbers)
-
-while index < length:
-    current_number = numbers[index]
-    if current_number > 0:
-        positive_numbers.append(current_number)
-    elif current_number < 0:
-        negative_numbers.append(current_number)
-    index += 1
+positive_numbers = list(filter(lambda x: x > 0, numbers))
+negative_numbers = list(filter(lambda x: x < 0, numbers))
 
 print(f"Positive numbers: {positive_numbers}\
       \nNegative numbers: {negative_numbers}")
