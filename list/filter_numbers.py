21bdc9a8a6f388ddd5a2ecd331e50ee1d84a0186
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-16 14:54:04 +0530

    [IMP] Separate positive and negative numbers using while loop

--- list/filter_numbers.py
+++ list/filter_numbers.py
@@ -4,12 +4,16 @@ numbers = [0, 1, 2, -3, 4, -5, 6, -1, 2, 3]
 
 positive_numbers = []
 negative_numbers = []
+index = 0
+length = len(numbers)
 
-for num in numbers:
-    if num > 0:
-        positive_numbers.append(num)
-    elif num < 0:
-        negative_numbers.append(num)
+while index < length:
+    current_number = numbers[index]
+    if current_number > 0:
+        positive_numbers.append(current_number)
+    elif current_number < 0:
+        negative_numbers.append(current_number)
+    index += 1
 
 print(f"Positive numbers: {positive_numbers}\
       \nNegative numbers: {negative_numbers}")
