b78291f9fa52c49e967792b148ec181a6edc0c3b
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-17 13:07:10 +0530

    [IMP] Find min. and max. value in tuple.

--- tuple/find_min_max.py
+++ tuple/find_min_max.py
@@ -1,5 +1,18 @@
 """Find min and max in tuple."""
 
 t = (1, 2, 3, 4, 5)
-print(f"Minimum value: {min(t)}")
-print(f"Maximum value: {max(t)}")
+
+if t:
+    min_value = t[0]
+    max_value = t[0]
+
+    for number in t:
+        if number < min_value:
+            min_value = number
+        if number > max_value:
+            max_value = number
+
+    print(f"Minimum value: {min_value}")
+    print(f"Maximum value: {max_value}")
+else:
+    print("Tuple is empty!")
