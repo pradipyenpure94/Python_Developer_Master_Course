704dc14828d19d80656168f5666a0f749b0c8af9
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-17 14:38:30 +0530

    [ADD] Implement second largest finder

---
+++ tuple/find_second_largest_number.py
@@ -0,0 +1,17 @@
+"""Find the second largest number in tuple."""
+
+t = (1, 2, 3, 4, 5, 6)
+
+if len(t) < 2:
+    print("Tuple must contain at least two elements.")
+else:
+    first_number = second_number = float('-inf')
+
+    for number in t:
+        if number > first_number:
+            second_number = first_number
+            first_number = number
+        elif number >= second_number and number != first_number:
+            second_number = number
+
+    print(f"Second largest number: {second_number}")
