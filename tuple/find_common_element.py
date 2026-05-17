310256edc9ea1404f06aac6013dbbbdeb4bb67b6
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-17 18:03:42 +0530

    [IMP] Find common elements in two tuple

--- tuple/find_common_element.py
+++ tuple/find_common_element.py
@@ -1,11 +1,15 @@
-"""Find common element in two tuple."""
+"""Find common elements in two tuple."""
+
 t1 = (1, 2, 3, 4)
 t2 = (3, 4, 5, 6)
 
 result = ()
+index = 0
 
-for i in t1:
-    if i in t2:
-        result += (i, )
+while index < len(t1):
+    current_number = t1[index]
+    if current_number in t2:
+        result += (current_number, )
+    index += 1
 
-print(f"Common element: {result}")
+print(f"Common elements: {result}")
