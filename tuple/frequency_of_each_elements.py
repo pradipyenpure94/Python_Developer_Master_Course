8b0ae70c126523577fc91bd67d609ac33d3fa996
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-17 16:42:11 +0530

    [IMP] Find frequency of each elements in tuple using while loop

--- tuple/frequency_of_each_elements.py
+++ tuple/frequency_of_each_elements.py
@@ -2,5 +2,11 @@
 
 numbers = (1, 2, 3, 4, 5, 6, 2, 5, 9, 2, 4, 7, 1, 5, 9, 3, 5, 4)
 
-for number in set(numbers):
-    print(number, "frequency of : ", numbers.count(number))
+unique_numbers = list(set(numbers))
+index = 0
+
+while index < len(unique_numbers):
+    current_number = unique_numbers[index]
+
+    print(current_number, "frequency of : ", numbers.count(current_number))
+    index += 1
