752133e7730a4e10fc507aeadb1d82016aef46a2
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-17 13:55:15 +0530

    [IMP] Count even numbers in tuple using while loop

--- tuple/count_even_numbers.py
+++ tuple/count_even_numbers.py
@@ -3,9 +3,13 @@
 t = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
 
 count = 0
+index = 0
+tuple_length = len(t)
 
-for number in t:
-    if number % 2 == 0:
+while index < tuple_length:
+    if t[index] % 2 == 0:
         count += 1
+
+    index += 1
 
 print(f"Number of even elements: {count}")
