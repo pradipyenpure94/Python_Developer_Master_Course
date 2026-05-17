0fc7ca04d9af5d964d093437cad706db9061f342
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-17 18:14:46 +0530

    [ADD] Find element appearing only once using while loop

--- tuple/count_element.py
+++ tuple/count_element.py
@@ -2,6 +2,10 @@
 
 t = (1, 2, 2, 3, 5, 6, 4, 4, 5, 8)
 
-for number in t:
-    if t.count(number) == 1:
-        print(number)
+index = 0
+
+while index < len(t):
+    currnt_number = t[index]
+    if t.count(currnt_number) == 1:
+        print(currnt_number)
+    index += 1
