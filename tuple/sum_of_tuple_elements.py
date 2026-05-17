99d629d2352f4a2d6705d5509d05c824004f101d
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-17 13:28:10 +0530

    [IMP] Calculate the sum of tuple elements using while loop.

--- tuple/sum_of_tuple_elements.py
+++ tuple/sum_of_tuple_elements.py
@@ -3,8 +3,12 @@
 t = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
 
 total = 0
+index = 0
+length = len(t)
 
-for number in t:
-    total += number
+while index < length:
+    total += t[index]
+
+    index += 1
 
 print(f"Sum of tuple elements: {total}")
