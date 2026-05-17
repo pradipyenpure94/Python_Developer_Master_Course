c4b67b16e42d32540bfbb89c501b8a3df1c4da9c
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-17 13:25:28 +0530

    [IMP] Calculate the sum of tuple elements using loop.

--- tuple/sum_of_tuple_elements.py
+++ tuple/sum_of_tuple_elements.py
@@ -1,5 +1,10 @@
 """Sum of tuple elements."""
 
 t = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
-total = sum(t)
+
+total = 0
+
+for number in t:
+    total += number
+
 print(f"Sum of tuple elements: {total}")
