8290313c2c1df883857d9f5ce870a0c0e0a63e1a
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-17 13:14:05 +0530

    [IMP] Find min. and max. value in tuple using while loop.

--- tuple/find_min_max.py
+++ tuple/find_min_max.py
@@ -5,12 +5,18 @@ t = (1, 2, 3, 4, 5)
 if t:
     min_value = t[0]
     max_value = t[0]
+    index = 0
+    length = len(t)
 
-    for number in t:
-        if number < min_value:
-            min_value = number
-        if number > max_value:
-            max_value = number
+    while index < length:
+        current_number = t[index]
+
+        if current_number < min_value:
+            min_value = current_number
+        if current_number > max_value:
+            max_value = current_number
+
+        index += 1
 
     print(f"Minimum value: {min_value}")
     print(f"Maximum value: {max_value}")
