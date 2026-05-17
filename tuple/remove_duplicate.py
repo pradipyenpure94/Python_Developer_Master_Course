32cd8b836d0d465b580492c6ac705c4781907fb4
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-17 17:20:14 +0530

    [IMP] Remove duplicate elements from tuple manually

--- tuple/remove_duplicate.py
+++ tuple/remove_duplicate.py
@@ -1,5 +1,9 @@
-"""Remove duplicate from tuple."""
+"""Remove duplicates from tuple."""
 
 t = (1, 2, 3, 4, 5, 6, 1, 2, 3)
-unique_tuple = tuple(set(t))
+unique_tuple = ()
+
+for number in t:
+    if number not in unique_tuple:
+        unique_tuple += (number,)
 print(f"Unique tuple: {unique_tuple}")
