1d7c6ef8965c78938a66caaba879163c0ee22bb9
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-18 14:59:24 +0530

    [IMP] Implement set intersection logic using loop

--- sets/find_common_elements.py
+++ sets/find_common_elements.py
@@ -3,5 +3,10 @@
 set1 = {1, 2, 3, 4}
 set2 = {3, 4, 5, 6}
 
-common_elements = set1.intersection(set2)
+common_elements = set()
+
+for number in set1:
+    if number in set2:
+        common_elements.add(number)
+
 print(f"Common elements: {common_elements}")
