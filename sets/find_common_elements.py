704d71a8011c9ffb77ce37f267227dd070e52410
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-18 15:03:24 +0530

    [IMP] Find common elements using set comprehension

--- sets/find_common_elements.py
+++ sets/find_common_elements.py
@@ -3,10 +3,5 @@
 set1 = {1, 2, 3, 4}
 set2 = {3, 4, 5, 6}
 
-common_elements = set()
-
-for number in set1:
-    if number in set2:
-        common_elements.add(number)
-
+common_elements = {number for number in set1 if number in set2}
 print(f"Common elements: {common_elements}")
