d3e971e0b6fe3bca63dddd0d3833050712757000
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-18 15:54:02 +0530

    [IMP] Find unique elements using set comprehension

--- sets/find_unique_element.py
+++ sets/find_unique_element.py
@@ -3,5 +3,6 @@
 set1 = {1, 2, 3, 4, 5}
 set2 = {2, 3, 6, 7}
 
-unique_elements = set1 ^ set2
+unique_elements = {number for number in set1.union(set2)
+                   if (number in set1) != (number in set2)}
 print(f"Unique elements: {unique_elements}")
