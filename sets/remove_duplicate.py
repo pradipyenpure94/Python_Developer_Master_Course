410b4fc3e9e71e2bafad9181cac368893bea8b42
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-18 14:46:05 +0530

    [IMP] Remove duplicates from list using while loop

--- sets/remove_duplicate.py
+++ sets/remove_duplicate.py
@@ -3,6 +3,14 @@
 numbers = [1, 2, 2, 3, 4, 4, 5]
 
 seen = set()
-unique_numbers = [number for number in numbers
-                  if not (number in seen or seen.add(number))]
+unique_numbers = []
+index = 0
+
+while index < len(numbers):
+    current_number = numbers[index]
+    if current_number not in seen:
+        unique_numbers.append(current_number)
+        seen.add(current_number)
+    index += 1
+
 print(f"Unique numbers: {unique_numbers}")
