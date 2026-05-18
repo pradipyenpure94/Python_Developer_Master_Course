2db04177d0efb7c22ce944e4bc5209ce38e6ff3d
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-18 14:37:55 +0530

    [IMP] Implement duplicate removal using list comprehension

--- sets/remove_duplicate.py
+++ sets/remove_duplicate.py
@@ -1,12 +1,8 @@
 """Remove duplicates from list."""
 
 numbers = [1, 2, 2, 3, 4, 4, 5]
-unique_numbers = []
+
 seen = set()
-
-for number in numbers:
-    if number not in seen:
-        unique_numbers.append(number)
-        seen.add(number)
-
+unique_numbers = [number for number in numbers
+                  if not (number in seen or seen.add(number))]
 print(f"Unique numbers: {unique_numbers}")
