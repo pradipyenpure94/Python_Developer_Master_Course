58510b8ae47c068553ffb3f2aa666cf8dbea65b6
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-18 15:16:21 +0530

    [IMP] Find common elements using while loop

--- sets/find_common_elements.py
+++ sets/find_common_elements.py
@@ -3,5 +3,14 @@
 set1 = {1, 2, 3, 4}
 set2 = {3, 4, 5, 6}
 
-common_elements = {number for number in set1 if number in set2}
+common_elements = set()
+list_set = list(set1)
+index = 0
+
+while index < len(list_set):
+    current_number = list_set[index]
+    if current_number in set2:
+        common_elements.add(current_number)
+    index += 1
+
 print(f"Common elements: {common_elements}")
