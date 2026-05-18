cbbb6ae59dffcf967af4213c86e1730bdc6de0e4
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-18 14:22:42 +0530

    [IMP] Remove duplicates from list

--- sets/remove_duplicate.py
+++ sets/remove_duplicate.py
@@ -1,5 +1,12 @@
 """Remove duplicates from list."""
 
 numbers = [1, 2, 2, 3, 4, 4, 5]
-unique_numbers = list(dict.fromkeys(numbers))
+unique_numbers = []
+seen = set()
+
+for number in numbers:
+    if number not in seen:
+        unique_numbers.append(number)
+        seen.add(number)
+
 print(f"Unique numbers: {unique_numbers}")
