8236ebe5f4657d622290543157dee51eaff5d056
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-19 19:49:39 +0530

    [IMP] Implement separate positive and negative numbers using filter() method

--- Phase2/filter_numbers.py
+++ Phase2/filter_numbers.py
@@ -2,7 +2,7 @@
 
 numbers = [-1, -2, 5, 6, 4, -9, -8, 4, 2, 3, -9]
 
-positive_numbers = [number for number in numbers if number > 0]
-negative_numbers = [number for number in numbers if number < 0]
+positive_numbers = list(filter(lambda number: number > 0, numbers))
+negative_numbers = list(filter(lambda number: number < 0, numbers))
 print(f"Positive numbers: {positive_numbers}")
 print(f"Negative numbers: {negative_numbers}")
