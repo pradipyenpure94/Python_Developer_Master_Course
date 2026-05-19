2a1acce51b2e6fa8f3470da9d7ed3f843e1ea675
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-19 13:09:52 +0530

    [IMP] Add program to find sum of all elements in list using while loop

--- Phase2/sum_of_list.py
+++ Phase2/sum_of_list.py
@@ -3,8 +3,9 @@
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 total = 0
+index = 0
 
-for number in numbers:
-    total += number
-
+while index < len(numbers):
+    total += numbers[index]
+    index += 1
 print(f"Sum of List: {total}")
