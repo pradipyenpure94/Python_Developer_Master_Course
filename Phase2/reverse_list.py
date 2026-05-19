b3ea1e6c2328a353e355c6144366f07fa0e205f0
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-19 13:57:35 +0530

    [IMP] Improve manual program to reverse list using append()

--- Phase2/reverse_list.py
+++ Phase2/reverse_list.py
@@ -1,5 +1,9 @@
 """Reverse a list."""
 
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-reversed_numbers = list(reversed(numbers))
+
+reversed_numbers = []
+for index in range(len(numbers) - 1, -1, -1):
+    reversed_numbers.append(numbers[index])
+
 print(f"Reversed List: {reversed_numbers}")
