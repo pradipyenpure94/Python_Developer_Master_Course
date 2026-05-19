31340f996a5102e5bdba362ef9f33e5a9d6a1cb2
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-19 14:37:01 +0530

    [IMP] Add program to sort list using bubble sort algorithm

--- Phase2/sort_list.py
+++ Phase2/sort_list.py
@@ -1,5 +1,15 @@
 """Sorted List"""
 
 numbers = [10, 50, 40, 20, 30, 50, 80, 90, 70, 60, 40, 20]
-sorted_numbers = sorted(numbers, reverse=True)
-print(f"Sorted numbers: {sorted_numbers}")
+
+for i in range(len(numbers)):
+    swapped = False
+    for j in range(len(numbers) - i - 1):
+        if numbers[j] > numbers[j + 1]:
+            numbers[j], numbers[j + 1] = numbers[j + 1], numbers[j]
+            swapped = True
+
+    if not swapped:
+        break
+
+print(f"Sorted numbers: {numbers}")
