1c76a4725dfd83ed684934280a609b3543e855b5
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-19 14:56:00 +0530

    [ADD] Add program to remove duplicate element using set()

---
+++ Phase2/remove_duplicate.py
@@ -0,0 +1,6 @@
+"""Remove duplicates element from list."""
+
+numbers = [10, 50, 40, 20, 30, 50, 80, 90, 70, 60, 40, 20]
+
+unique_numbers = list(set(numbers))
+print(f"Unique numbers: {unique_numbers}")
