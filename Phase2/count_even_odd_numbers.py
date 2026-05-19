88cbe220d0aa7bf2301abdd93a1b4b826d5971b8
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-19 16:10:21 +0530

    [IMP] Add program to count even and odd numbers counting logic using while loop

--- Phase2/count_even_odd_numbers.py
+++ Phase2/count_even_odd_numbers.py
@@ -4,13 +4,17 @@ numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
 
 count_even_numbers = 0
 count_odd_numbers = 0
+index = 0
 
-for number in numbers:
-    if isinstance(number, int):
-        if number % 2 == 0:
+while index < len(numbers):
+    current_number = numbers[index]
+
+    if isinstance(current_number, int):
+        if current_number % 2 == 0:
             count_even_numbers += 1
         else:
             count_odd_numbers += 1
+    index += 1
 
 print(f"Count even numbers: {count_even_numbers}")
 print(f"Count odd numbers: {count_odd_numbers}")
