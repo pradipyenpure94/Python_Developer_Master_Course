7a1f9bb87a55e61de2f137fa85e6d30cf1a57866
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-19 13:36:46 +0530

    [IMP] Add program to reverse list using reversed() method

--- Phase2/reverse_list.py
+++ Phase2/reverse_list.py
@@ -1,5 +1,5 @@
 """Reverse a list."""
 
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-numbers.reverse()
-print(f"Reversed List: {numbers}")
+reversed_numbers = list(reversed(numbers))
+print(f"Reversed List: {reversed_numbers}")
