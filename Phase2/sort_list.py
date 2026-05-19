0f599c5c9f32f6d8d84ee8ee984d0d284cf62435
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-19 14:46:13 +0530

    [IMP] Sorted list using bubble sort

--- Phase2/sort_list.py
+++ Phase2/sort_list.py
@@ -2,14 +2,24 @@
 
 numbers = [10, 50, 40, 20, 30, 50, 80, 90, 70, 60, 40, 20]
 
-for i in range(len(numbers)):
+i = 0
+list_length = len(numbers)
+
+while i < list_length:
     swapped = False
-    for j in range(len(numbers) - i - 1):
+
+    j = 0
+
+    while j < (list_length - i - 1):
         if numbers[j] > numbers[j + 1]:
             numbers[j], numbers[j + 1] = numbers[j + 1], numbers[j]
             swapped = True
 
+        j += 1
+
     if not swapped:
         break
+
+    i += 1
 
 print(f"Sorted numbers: {numbers}")
