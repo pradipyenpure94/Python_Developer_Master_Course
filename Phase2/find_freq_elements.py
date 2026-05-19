77f1767a14bfb90f55b54fa4b988f7b715625e8a
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-19 19:29:31 +0530

    [IMP] Improve logic to find frequency of each element using Counter()

--- Phase2/find_freq_elements.py
+++ Phase2/find_freq_elements.py
@@ -1,11 +1,10 @@
 """Find frequency of each element."""
 
+from collections import Counter
+
 numbers = [1, 2, 3, 1, 4, 7, 2, 5, 8, 3, 6, 9, 9, 8, 7, 4, 5, 6, 7, 4, 4, 4]
 
-freq = {}
-
-for number in numbers:
-    freq[number] = freq.get(number, 0) + 1
+freq = Counter(numbers)
 
 for number, count in freq.items():
     print(f"Frequency of {number} is {count}")
