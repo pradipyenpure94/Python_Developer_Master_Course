c8d5c043e3ce39545916fe58c1592783755c81bc
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-19 15:00:06 +0530

    [IMP] Add program to find unique numbers in list using dict.fromkeys()

--- Phase2/remove_duplicate.py
+++ Phase2/remove_duplicate.py
@@ -2,5 +2,5 @@
 
 numbers = [10, 50, 40, 20, 30, 50, 80, 90, 70, 60, 40, 20]
 
-unique_numbers = list(set(numbers))
+unique_numbers = list(dict.fromkeys(numbers))
 print(f"Unique numbers: {unique_numbers}")
