b319b3f1f615603d553340b3cebd154df0201e97
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-19 14:03:21 +0530

    [IMP] Add while loop program to implement reverse list using append()

--- Phase2/reverse_list.py
+++ Phase2/reverse_list.py
@@ -3,7 +3,10 @@
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 reversed_numbers = []
-for index in range(len(numbers) - 1, -1, -1):
+index = len(numbers) - 1
+
+while index >= 0:
     reversed_numbers.append(numbers[index])
+    index -= 1
 
 print(f"Reversed List: {reversed_numbers}")
