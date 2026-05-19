19cf67e53c318854ee2eb2281a201b4f866c955e
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-19 15:10:05 +0530

    [IMP] Add program to removal duplicates number using while loop

--- Phase2/remove_duplicate.py
+++ Phase2/remove_duplicate.py
@@ -4,10 +4,13 @@ numbers = [10, 50, 40, 20, 30, 50, 80, 90, 70, 60, 40, 20]
 
 unique_numbers = []
 seen = set()
+index = 0
 
-for number in numbers:
-    if number not in seen:
-        seen.add(number)
-        unique_numbers.append(number)
+while index < len(numbers):
+    current_number = numbers[index]
+    if current_number not in seen:
+        seen.add(current_number)
+        unique_numbers.append(current_number)
+    index += 1
 
 print(f"Unique numbers: {unique_numbers}")
