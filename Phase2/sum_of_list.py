1026ab8ef426b6eea0436f7f451da26a9e48a8c1
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-19 13:04:06 +0530

    [IMP] Add program to calculate sum of all elements in list using for loop

--- Phase2/sum_of_list.py
+++ Phase2/sum_of_list.py
@@ -1,5 +1,10 @@
 """Sum of all elements in list."""
 
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-total = sum(numbers)
+
+total = 0
+
+for number in numbers:
+    total += number
+
 print(f"Sum of List: {total}")
