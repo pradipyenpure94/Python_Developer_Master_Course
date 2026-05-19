e7b140aa09b0d7317dd5a3b399e980b697f9fc8b
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-19 12:53:08 +0530

    [IMP] Add reduce based program to find minimum and maximum element in list

--- Phase2/find_min_max_element.py
+++ Phase2/find_min_max_element.py
@@ -1,19 +1,12 @@
 """Find minimum and maximum element"""
 
+from functools import reduce
+
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 if numbers:
-    min_number = numbers[0]
-    max_number = numbers[0]
-    index = 1
-
-    while index < len(numbers):
-        current_number = numbers[index]
-        if current_number < min_number:
-            min_number = current_number
-        elif current_number > max_number:
-            max_number = current_number
-        index += 1
+    min_number = reduce(lambda x, y: x if x < y else y, numbers)
+    max_number = reduce(lambda x, y: x if x > y else y, numbers)
 
     print(f"Minimum element: {min_number}")
     print(f"Maximum element: {max_number}")
