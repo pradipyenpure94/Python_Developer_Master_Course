a477dde8a3dc1a370934d6010adee865e8aaa903
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-19 15:31:20 +0530

    [IMP] Add program to remove duplicates number using list comprehension

--- Phase2/remove_duplicate.py
+++ Phase2/remove_duplicate.py
@@ -2,15 +2,7 @@
 
 numbers = [10, 50, 40, 20, 30, 50, 80, 90, 70, 60, 40, 20]
 
-unique_numbers = []
 seen = set()
-index = 0
-
-while index < len(numbers):
-    current_number = numbers[index]
-    if current_number not in seen:
-        seen.add(current_number)
-        unique_numbers.append(current_number)
-    index += 1
-
+unique_numbers = [number for number in numbers
+                  if not (number in seen or seen.add(number))]
 print(f"Unique numbers: {unique_numbers}")
