6204d35ffd2ce2820f3a96f856c1b95bad58713d
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-19 14:09:53 +0530

    [IMP] Add program to reverse list using list slicing

--- Phase2/reverse_list.py
+++ Phase2/reverse_list.py
@@ -2,11 +2,5 @@
 
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-reversed_numbers = []
-index = len(numbers) - 1
-
-while index >= 0:
-    reversed_numbers.append(numbers[index])
-    index -= 1
-
+reversed_numbers = numbers[::-1]
 print(f"Reversed List: {reversed_numbers}")
