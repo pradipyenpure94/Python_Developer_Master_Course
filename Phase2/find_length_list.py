6773db17a6cb72603a57300e8dd66876dfd918d3
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-19 12:23:00 +0530

    [IMP] Add while loop program to find length of list.

--- Phase2/find_length_list.py
+++ Phase2/find_length_list.py
@@ -2,8 +2,10 @@
 
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 length_of_list = 0
+index = 0
 
-for _ in numbers:
+while index < len(numbers):
     length_of_list += 1
+    index += 1
 
 print(f"Length of List: {length_of_list}")
