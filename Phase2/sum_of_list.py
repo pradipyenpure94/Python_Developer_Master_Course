ce9408183f83c136ddf08540c4ed748ae8dab2cd
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-19 13:13:56 +0530

    [IMP] Add program to calculate sum of all elements in list using reduce()

--- Phase2/sum_of_list.py
+++ Phase2/sum_of_list.py
@@ -1,11 +1,8 @@
 """Sum of all elements in list."""
 
+from functools import reduce
+
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-total = 0
-index = 0
-
-while index < len(numbers):
-    total += numbers[index]
-    index += 1
+total = reduce(lambda x, y: x + y, numbers)
 print(f"Sum of List: {total}")
