5fc9b008daa0f7e69fcd33320a6664275852dcb2
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-19 16:43:39 +0530

    [IMP] Add program to find second largest number using while loop

--- Phase2/find_second_largest.py
+++ Phase2/find_second_largest.py
@@ -3,13 +3,20 @@
 numbers = [102, 0, 30, 4, 0, 50, 607, 0, 80, 90, 100]
 
 largest_number = second_largest_number = float('-inf')
+index = 0
+list_length = len(numbers)
 
-for number in numbers:
-    if number > largest_number:
+while index < list_length:
+    current_number = numbers[index]
+
+    if current_number > largest_number:
         second_largest_number = largest_number
-        largest_number = number
-    elif number >= second_largest_number and number != largest_number:
-        second_largest_number = number
+        largest_number = current_number
+    elif current_number >= second_largest_number and \
+        current_number != largest_number:
+        second_largest_number = current_number
+
+    index += 1
 
 if second_largest_number != float('-inf'):
     print(f"Second Largest number: {second_largest_number}")
