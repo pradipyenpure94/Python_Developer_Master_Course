e99719094032b894d9a4b2d88898a0efec7ad3f9
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-19 15:41:25 +0530

    [IMP] Add program to remove duplicates number using filter()

--- Phase2/remove_duplicate.py
+++ Phase2/remove_duplicate.py
@@ -3,6 +3,7 @@
 numbers = [10, 50, 40, 20, 30, 50, 80, 90, 70, 60, 40, 20]
 
 seen = set()
-unique_numbers = [number for number in numbers
-                  if not (number in seen or seen.add(number))]
+unique_numbers = list(filter(
+    lambda number: number not in seen and not seen.add(number), numbers))
+
 print(f"Unique numbers: {unique_numbers}")
