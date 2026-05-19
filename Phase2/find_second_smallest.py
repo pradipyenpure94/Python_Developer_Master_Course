ffc39c17502c675959696c6727b94674f1487fc7
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-19 17:25:34 +0530

    [ADD] Add program to find second smallest number

---
+++ Phase2/find_second_smallest.py
@@ -0,0 +1,17 @@
+"""Find the second smallest number from list."""
+
+numbers = [40, 50, 60, 20, 30, 10, 80, 90, 70]
+
+smallest = second_smallest = float('inf')
+
+for number in numbers:
+    if number < smallest:
+        second_smallest = smallest
+        smallest = number
+    elif number < second_smallest and smallest != number:
+        second_smallest = number
+
+if second_smallest != float('inf'):
+    print(f"Second smallest number: {second_smallest}")
+else:
+    print("Second smallest number not found (Numbers may be equal)")
