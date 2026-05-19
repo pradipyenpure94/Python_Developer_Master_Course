51f526c42fb062c702b93228d03305c5d1d7f168
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-19 14:16:21 +0530

    [IMP] Add program to reverse list using indexing

--- Phase2/reverse_list.py
+++ Phase2/reverse_list.py
@@ -2,5 +2,12 @@
 
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-reversed_numbers = numbers[::-1]
-print(f"Reversed List: {reversed_numbers}")
+left = 0
+right = len(numbers) - 1
+
+while left < right:
+    numbers[left], numbers[right] = numbers[right], numbers[left]
+    left += 1
+    right -= 1
+
+print(f"Reversed List: {numbers}")
