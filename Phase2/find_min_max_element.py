8b93359103f1fcfd4397a4a7134f79b77df8aa61
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-19 12:42:10 +0530

    [IMP] Add program to find minimum and maximum number in list using loop

--- Phase2/find_min_max_element.py
+++ Phase2/find_min_max_element.py
@@ -3,10 +3,16 @@
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 if numbers:
-    minimum_element = min(numbers)
-    maximum_element = max(numbers)
+    min_number = numbers[0]
+    max_number = numbers[0]
 
-    print(f"Minimum element: {minimum_element}")
-    print(f"Maximum element: {maximum_element}")
+    for number in numbers[1:]:
+        if number < min_number:
+            min_number = number
+        if number > max_number:
+            max_number = number
+
+    print(f"Minimum element: {min_number}")
+    print(f"Maximum element: {max_number}")
 else:
     print("List is empty!")
