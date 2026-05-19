a3bfb044d755934ddf0c7e1bc4073a4788e02dff
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-19 15:05:38 +0530

    [IMP] Add program to remove duplicates number using append()

--- Phase2/remove_duplicate.py
+++ Phase2/remove_duplicate.py
@@ -2,5 +2,12 @@
 
 numbers = [10, 50, 40, 20, 30, 50, 80, 90, 70, 60, 40, 20]
 
-unique_numbers = list(dict.fromkeys(numbers))
+unique_numbers = []
+seen = set()
+
+for number in numbers:
+    if number not in seen:
+        seen.add(number)
+        unique_numbers.append(number)
+
 print(f"Unique numbers: {unique_numbers}")
