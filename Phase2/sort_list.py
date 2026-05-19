51d144980cb3434a6fc291f635ff622a423c70a7
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-19 14:26:06 +0530

    [IMP] Add program to sort list using sorted()

--- Phase2/sort_list.py
+++ Phase2/sort_list.py
@@ -1,5 +1,5 @@
 """Sorted List"""
 
 numbers = [10, 50, 40, 20, 30, 50, 80, 90, 70, 60, 40, 20]
-numbers.sort(reverse=True)
-print(f"Sorted numbers: {numbers}")
+sorted_numbers = sorted(numbers, reverse=True)
+print(f"Sorted numbers: {sorted_numbers}")
