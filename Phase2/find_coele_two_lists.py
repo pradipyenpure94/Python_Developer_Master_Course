f4ac24a05ef83ef511d579b930f522abf04767c9
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-19 18:19:36 +0530

    [IMP] Implement to find common elements in two lists

--- Phase2/find_coele_two_lists.py
+++ Phase2/find_coele_two_lists.py
@@ -3,12 +3,16 @@
 odd = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
 prime = [2, 3, 5, 7, 11, 13, 17, 19]
 
-odd_set = set(odd)
 prime_set = set(prime)
 common_elements = []
+index = 0
+odd_list = list(set(odd))
 
-for number in odd_set:
-    if number in prime_set:
-        common_elements.append(number)
+
+while index < len(odd_list):
+    current_number = odd_list[index]
+    if current_number in prime_set:
+        common_elements.append(current_number)
+    index += 1
 
 print(f"Common elements of two lists: {common_elements}")
