e0fdc5178a79c19a20b68c557ba19e1e89f4f703
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-19 16:04:59 +0530

    [IMP] Implement count even and odd numbers logic using loop

--- Phase2/count_even_odd_numbers.py
+++ Phase2/count_even_odd_numbers.py
@@ -2,10 +2,15 @@
 
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
 
-count_even_numbers = sum(1 for number in numbers
-                         if isinstance(number, int) and number % 2 == 0)
-count_odd_numbers = sum(1 for number in numbers
-                        if isinstance(number, int) and number % 2 == 1)
+count_even_numbers = 0
+count_odd_numbers = 0
+
+for number in numbers:
+    if isinstance(number, int):
+        if number % 2 == 0:
+            count_even_numbers += 1
+        else:
+            count_odd_numbers += 1
 
 print(f"Count even numbers: {count_even_numbers}")
 print(f"Count odd numbers: {count_odd_numbers}")
