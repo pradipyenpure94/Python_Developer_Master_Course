1a3df46dbf850a279c497139b2e5c5e139f035dc
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-20 13:40:34 +0530

    [IMP] Move zeros to end using sort() and lambda expression

--- Phase2/move_all_zeros_to_end.py
+++ Phase2/move_all_zeros_to_end.py
@@ -3,13 +3,7 @@
 numbers = [1, 2, 0, 2, 3, 0, 4, 5, 6, 0, 7, 8, 9, 0, 1, 4, 5]
 
 if numbers:
-    left = 0
-
-    for right, _ in enumerate(numbers):
-        if numbers[right] != 0:
-            numbers[left], numbers[right] = numbers[right], numbers[left]
-            left += 1
-
+    numbers.sort(key=lambda x: x == 0)
     print(f"Move all zeros to end: {numbers}")
 else:
     print("List is empty!")
