3bfe3152091829ad07ce8403112ecb236d39400d
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-20 14:33:19 +0530

    [IMP] Implement check if list is palindrome using reversed()

--- Phase2/check_list_palindrome.py
+++ Phase2/check_list_palindrome.py
@@ -1,21 +1,8 @@
 """Check if list is palindrome."""
 
-numbers = [1, 2, 1, 1]
+numbers = [1, 2, 3, 1]
 
-left = 0
-right = len(numbers) - 1
-
-is_palindrome = True
-
-while left < right:
-    if numbers[left] != numbers[right]:
-        is_palindrome = False
-        break
-
-    left += 1
-    right -= 1
-
-if is_palindrome:
-    print(f"{numbers} is palindrome.")
+if list(reversed(numbers)) == numbers:
+    print("List is a palindrome.")
 else:
-    print(f"{numbers} is not palindrome.")
+    print("List is not a palindrome.")
