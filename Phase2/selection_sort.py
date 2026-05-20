af902284ad79f2a4fbd53643e362b673776b0365
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-20 18:13:59 +0530

    [IMP] Improve selction sort logic with minimum element using while loop

--- Phase2/selection_sort.py
+++ Phase2/selection_sort.py
@@ -4,11 +4,20 @@ numbers = [10, 20, 50, 60, 40, 80, 70, 90]
 
 numbers_count = len(numbers)
 
-for i in range(numbers_count - 1):
+i = 0
+
+while i < numbers_count - 1:
     min_index = i
-    for j in range(i + 1, numbers_count):
+    j = i + 1
+
+    while j < numbers_count:
         if numbers[j] < numbers[min_index]:
             min_index = j
+
+        j += 1
+
     numbers[i], numbers[min_index] = numbers[min_index], numbers[i]
+
+    i += 1
 
 print(f"Sorted numbers: {numbers}")
