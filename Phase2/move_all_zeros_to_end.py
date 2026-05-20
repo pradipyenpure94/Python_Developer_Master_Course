56581496a6c281ce29952a57035f178563a49a57
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-20 12:58:45 +0530

    [IMP] Shift all zeros to end using loop

--- Phase2/move_all_zeros_to_end.py
+++ Phase2/move_all_zeros_to_end.py
@@ -1,6 +1,15 @@
 """Move all zeros to end."""
 
 numbers = [1, 2, 0, 2, 3, 0, 4, 5, 6, 0, 7, 8, 9, 0, 1, 4, 5]
-move_zeros_to_end = [number for number in numbers if number != 0] +\
-                    [number for number in numbers if number == 0]
+
+non_zeros_numbers = []
+zeros_count = 0
+
+for number in numbers:
+    if number != 0:
+        non_zeros_numbers.append(number)
+    else:
+        zeros_count += 1
+
+move_zeros_to_end = non_zeros_numbers + [0] * zeros_count
 print(f"Move all zeros to end: {move_zeros_to_end}")
