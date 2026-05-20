9b16f75ebcb57b80088ca581ac71650a8929f5f0
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-20 13:03:35 +0530

    [IMP] Improve variable name and edge case handling

--- Phase2/move_all_zeros_to_end.py
+++ Phase2/move_all_zeros_to_end.py
@@ -2,14 +2,17 @@
 
 numbers = [1, 2, 0, 2, 3, 0, 4, 5, 6, 0, 7, 8, 9, 0, 1, 4, 5]
 
-non_zeros_numbers = []
-zeros_count = 0
+if numbers:
+    non_zero_numbers = []
+    zeros_count = 0
 
-for number in numbers:
-    if number != 0:
-        non_zeros_numbers.append(number)
-    else:
-        zeros_count += 1
+    for number in numbers:
+        if number != 0:
+            non_zero_numbers.append(number)
+        else:
+            zeros_count += 1
 
-move_zeros_to_end = non_zeros_numbers + [0] * zeros_count
-print(f"Move all zeros to end: {move_zeros_to_end}")
+    move_zeros_to_end = non_zero_numbers + [0] * zeros_count
+    print(f"Move all zeros to end: {move_zeros_to_end}")
+else:
+    print("List is empty!")
