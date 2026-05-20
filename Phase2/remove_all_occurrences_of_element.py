0fd40b17822fc1c07cfa73ba35399ed113cef853
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-20 20:08:40 +0530

    [IMP] Implement remove all occurrences of element from list using loop based filtering

--- Phase2/remove_all_occurrences_of_element.py
+++ Phase2/remove_all_occurrences_of_element.py
@@ -3,6 +3,11 @@
 numbers = [1, 2, 3, 2, 5, 6, 4, 2, 0, 8, 5, 0, 7, 9]
 target = 2
 
-print(f"Numbers: {numbers}")
-filtered_numbers = list(filter(lambda number:  number != target, numbers))
-print(f"Remove {target} from list: {filtered_numbers}")
+filtered_numbers = []
+
+for number in numbers:
+    if number != target:
+        filtered_numbers.append(number)
+
+print(f"Before removing numbers: {numbers}")
+print(f"After removing {target} from list: {filtered_numbers}")
