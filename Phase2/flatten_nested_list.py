5183b0b9ba7004b299e7f98ef5685a07b400f966
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-21 13:23:13 +0530

    [IMP] Refactor recursive nested list flattening into pure reusable function.

--- Phase2/flatten_nested_list.py
+++ Phase2/flatten_nested_list.py
@@ -1,6 +1,25 @@
 """Flatten nested list."""
 
-nested_list = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+from typing import Any
 
-flat_list = [item for sublist in nested_list for item in sublist]
-print(f"Flattened List: {flat_list}")
+data = [1, 2, [1, 2, 3], [4, 5, 6], [7, 8, 9, [10, 11, 12]]]
+
+
+def flatten(nested_list: list[Any]) -> list[Any]:
+    """Iterate sublist into single list.
+    Args:
+        nested_list (list[Any]): Input as sublist or int or float,..
+    Returns:
+        list[Any]: Flattened list
+    """
+    flat = []
+
+    for item in nested_list:
+        if isinstance(item, list):
+            flat.extend(flatten(item))
+        else:
+            flat.append(item)
+    return flat
+
+
+print(f"Flattened List: {flatten(nested_list=data)}")
