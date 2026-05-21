b3c89f7f6f0f375e540343c16917efc4c60654a6
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-21 14:15:18 +0530

    [IMP] Implement matrix addition using nested list comprehension

--- Phase2/matrix_addition.py
+++ Phase2/matrix_addition.py
@@ -8,12 +8,5 @@ y = [[5, 8, 1],
      [6, 7, 3],
      [4, 5, 9]]
 
-result = []
-
-for i, _ in enumerate(len(x)):
-    row = []
-    for j in range(len(x[0])):
-        row.append((x[i][j] + y[i][j]))
-    result.append(row)
-
+result = [[x[i][j] + y[i][j] for j in range(len(x[0]))] for i in range(len(x))]
 print(f"Matrix addition: {result}")
