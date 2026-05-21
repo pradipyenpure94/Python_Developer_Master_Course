2b2bc42986bd7c9e75eb5a4564dd32b2af75bea5
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-21 12:39:04 +0530

    [IMP] Remove all occurrences of element using while loop

--- Phase2/remove_all_occurrences_of_element.py
+++ Phase2/remove_all_occurrences_of_element.py
@@ -4,10 +4,13 @@ numbers = [1, 2, 3, 2, 5, 6, 4, 2, 0, 8, 5, 0, 7, 9]
 target = 2
 
 filtered_numbers = []
+index = 0
 
-for number in numbers:
-    if number != target:
-        filtered_numbers.append(number)
+while index < len(numbers):
+    current_number = numbers[index]
+    if current_number != target:
+        filtered_numbers.append(current_number)
+    index += 1
 
 print(f"Before removing numbers: {numbers}")
 print(f"After removing {target} from list: {filtered_numbers}")
