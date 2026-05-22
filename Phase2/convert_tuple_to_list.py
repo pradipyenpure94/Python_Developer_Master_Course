f3f3b5d29e7e6debbcd8108a3a83661ce27a68ff
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-22 16:57:44 +0530

    [IMP] Convert tuple to list using list comprehension

--- Phase2/convert_tuple_to_list.py
+++ Phase2/convert_tuple_to_list.py
@@ -1,4 +1,6 @@
 """Convert tuple to list."""
 
 numbers = (2, 4, 6, 8, 10)
-print(f"Converted tuple to list: {list(numbers)}")
+
+numbers_list = [number for number in numbers]
+print(f"Converted tuple to list: {numbers_list}")
