21c421b8b61350f1d40b577b506c82c17199dd18
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-22 18:49:17 +0530

    [IMP] Concatenation of two tuples using unpacking (*)

--- Phase2/concatenate_two_tuple.py
+++ Phase2/concatenate_two_tuple.py
@@ -3,5 +3,5 @@
 even = (2, 4, 6, 8, 10)
 odd = (1, 3, 5, 7, 9)
 
-numbers = even + odd
-print(f"Concatenation of two tuple is: {numbers}")
+numbers = (*even, *odd)
+print(f"Concatenation of two tuples is: {numbers}")
