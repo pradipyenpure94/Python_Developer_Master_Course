ddd11ed358ffd44d089e047fe3c0b391151037bd
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-22 14:58:38 +0530

    [IMP] Find duplicate numbers without using set

--- Phase2/find_duplicate_numbers.py
+++ Phase2/find_duplicate_numbers.py
@@ -1,14 +1,10 @@
 """Find duplicate numbers without using set."""
 
 numbers = [1, 2, 3, 4, 5, 6, 8, 5, 2, 1, 5, 9, 3, 5, 7]
-duplicate_numbers = []
 freq = {}
 
 for number in numbers:
     freq[number] = freq.get(number, 0) + 1
 
-for number, count in freq.items():
-    if count > 1:
-        duplicate_numbers.append(number)
-
+duplicate_numbers = [number for number, count in freq.items() if count > 1]
 print(f"Duplicate numbers: {duplicate_numbers}")
