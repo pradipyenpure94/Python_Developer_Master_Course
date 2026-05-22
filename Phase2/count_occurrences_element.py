2947ee8c89f2b13c1accb0ffde5dda84cc1031ae
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-22 18:00:45 +0530

    [IMP] Add program to counting frequency of element using while loop

--- Phase2/count_occurrences_element.py
+++ Phase2/count_occurrences_element.py
@@ -3,5 +3,14 @@
 numbers = (1, 2, 3, 4, 5, 6, 1, 5, 9, 7, 8, 9, 1)
 target = 1
 
-count = sum(number == target for number in numbers)
+count = 0
+index = 0
+
+while index < len(numbers):
+    number = numbers[index]
+    if number == target:
+        count += 1
+
+    index += 1
+
 print(f"{target} appeared {count} times.")
