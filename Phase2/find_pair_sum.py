9b3914f7f7a8609a1af96670ddda325975bc32cd
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-22 14:24:32 +0530

    [IMP] Optimized program to find pairs with given sum suing set comprehension

--- Phase2/find_pair_sum.py
+++ Phase2/find_pair_sum.py
@@ -4,13 +4,10 @@ numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 2, 1]
 target = 3
 
 seen = set()
-pairs = set()
-
-for number in numbers:
-    diff = target - number
-    if diff in seen:
-        pair = tuple(sorted((diff, number)))
-        pairs.add(pair)
-    seen.add(number)
+pairs = {
+    tuple(sorted((number, target - number)))
+    for number in numbers
+    if (target - number) in seen or not seen.add(number)
+}
 
 print(f"Sum pairs: {pairs}")
