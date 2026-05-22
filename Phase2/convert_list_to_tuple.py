5517cbc74486a12955292dd86d72bff5b3e0dba5
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-22 17:27:59 +0530

    [IMP] Implement list to tuple conversion using while loop

--- Phase2/convert_list_to_tuple.py
+++ Phase2/convert_list_to_tuple.py
@@ -2,5 +2,12 @@
 
 numbers = [2, 4, 6, 8, 10]
 
-numbers_tuple = (*numbers,)
+numbers_tuple = ()
+index = 0
+
+while index < len(numbers):
+    number = numbers[index]
+    numbers_tuple += (number, )
+    index += 1
+
 print(f"Converted list to tuple: {numbers_tuple}")
