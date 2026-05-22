aa73dc9c6eef29a3d34ac6e8ad59ed10f033b7a1
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-22 19:36:58 +0530

    [ADD] Add program to swap values using tuple unpacking

---
+++ Phase2/swap_values.py
@@ -0,0 +1,10 @@
+"""Swap values using tuple unpacking."""
+
+num1, num2 = 10, 20
+print("Before swap:")
+print(f"Num1: {num1}")
+print(f"Num2: {num2}")
+num1, num2 = num2, num1
+print("After swap:")
+print(f"Num1: {num1}")
+print(f"Num2: {num2}")
