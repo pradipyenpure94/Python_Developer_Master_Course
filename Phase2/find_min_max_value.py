6e30c0540020196373a5ace7af3a3d960781da1f
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-22 15:55:23 +0530

    [ADD] Add program to find minimum and maximum value using built-in functions

---
+++ Phase2/find_min_max_value.py
@@ -0,0 +1,7 @@
+"""Find minimum and maximum value."""
+
+numbers = [1, 4, 7, 8, 5, 2, 3, 6, 9]
+
+min_value, max_value = min(numbers), max(numbers)
+print(f"Minimum value: {min_value}")
+print(f"Maximum value: {max_value}")
