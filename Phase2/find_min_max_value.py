76caf5dcc91654669c21d96ab7c6b629c2147af0
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-22 16:06:40 +0530

    [IMP] Implement to find minimum and maximum value using loop

--- Phase2/find_min_max_value.py
+++ Phase2/find_min_max_value.py
@@ -2,6 +2,17 @@
 
 numbers = [1, 4, 7, 8, 5, 2, 3, 6, 9]
 
-min_value, max_value = min(numbers), max(numbers)
-print(f"Minimum value: {min_value}")
-print(f"Maximum value: {max_value}")
+if numbers:
+    min_value = numbers[0]
+    max_value = numbers[0]
+
+    for number in numbers[1:]:
+        if number < min_value:
+            min_value = number
+        elif number > max_value:
+            max_value = number
+
+    print(f"Minimum value: {min_value}")
+    print(f"Maximum value: {max_value}")
+else:
+    print("List is empty.")
