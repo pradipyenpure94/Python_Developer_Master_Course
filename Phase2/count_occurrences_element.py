7aed0d7c4d6cad22c26642e9c95a414902bfb1a3
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-22 17:55:37 +0530

    [IMP] Implement counting frequency of element using generator expression

--- Phase2/count_occurrences_element.py
+++ Phase2/count_occurrences_element.py
@@ -3,10 +3,5 @@
 numbers = (1, 2, 3, 4, 5, 6, 1, 5, 9, 7, 8, 9, 1)
 target = 1
 
-count = 0
-
-for number in numbers:
-    if number == target:
-        count += 1
-
+count = sum(number == target for number in numbers)
 print(f"{target} appeared {count} times.")
