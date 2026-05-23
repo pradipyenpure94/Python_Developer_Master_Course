87dc8f9dd2da8d6da17c962d9ff18fcbe2031650
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-23 13:05:23 +0530

    [IMP] find repeated items from tuple using list comprehension

--- Phase2/find_repeated_items.py
+++ Phase2/find_repeated_items.py
@@ -2,15 +2,8 @@
 
 numbers = (1, 2, 3, 1, 5, 9, 2, 5, 8, 3, 4, 7, 2, 5, 9, 8, 5, 4, 2, 1)
 
-freq = dict()
-index = 0
-
-while index < len(numbers):
-    number = numbers[index]
-    freq[number] = freq.get(number, 0) + 1
-    index += 1
-
-repeated_items = [num
-                  for num, count in freq.items()
-                  if count > 1]
+repeated_items = [number
+                  for index, number in enumerate(numbers)
+                  if numbers.count(number) > 1
+                  and number not in numbers[:index]]
 print(f"Repeated items: {repeated_items}")
