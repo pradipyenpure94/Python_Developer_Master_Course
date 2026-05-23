299464b0c4467d01eee6bafe8393f5e96d3634c9
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-23 17:32:37 +0530

    [IMP] Create nested tuple using for loop

--- Phase2/create_nested_tuple.py
+++ Phase2/create_nested_tuple.py
@@ -1,4 +1,9 @@
 """Create nested tuple."""
 
-nested_tuple = tuple((number, number ** 2) for number in range(1, 6))
+nested_list = []
+
+for number in range(1, 6):
+    nested_list.append((number, number ** 2))
+
+nested_tuple = tuple(nested_list)
 print(f"Nested tuple: {nested_tuple}")
