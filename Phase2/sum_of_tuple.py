f7bc3b0c40c8b4d074f73fac3587574d74827f07
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-23 18:25:21 +0530

    [IMP] Calculate tuple element sum using loop

--- Phase2/sum_of_tuple.py
+++ Phase2/sum_of_tuple.py
@@ -1,5 +1,10 @@
 """Find sum of tuple elements."""
 
 numbers = (1, 2, 3, 4, 5, 6)
-total = sum(numbers)
+
+total = 0
+
+for number in numbers:
+    total += number
+
 print(f"Sum of numbers (tuple): {total}")
