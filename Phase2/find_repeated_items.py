ef45094ae3153e748c77f956c5d8aa0097a45cf5
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-23 13:16:17 +0530

    [IMP] Find repeated items from tuple for loop

--- Phase2/find_repeated_items.py
+++ Phase2/find_repeated_items.py
@@ -2,8 +2,10 @@
 
 numbers = (1, 2, 3, 1, 5, 9, 2, 5, 8, 3, 4, 7, 2, 5, 9, 8, 5, 4, 2, 1)
 
-repeated_items = [number
-                  for index, number in enumerate(numbers)
-                  if numbers.count(number) > 1
-                  and number not in numbers[:index]]
+repeated_items = []
+
+for index, number in enumerate(numbers):
+    if (numbers.count(number) > 1 and number not in numbers[:index]):
+        repeated_items.append(number)
+
 print(f"Repeated items: {repeated_items}")
