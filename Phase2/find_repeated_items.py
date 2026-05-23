110b9324c969bb562edb85724e1621fe8078d565
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-23 12:58:16 +0530

    [IMP] Find repeated items from tuple using while loop

--- Phase2/find_repeated_items.py
+++ Phase2/find_repeated_items.py
@@ -3,9 +3,12 @@
 numbers = (1, 2, 3, 1, 5, 9, 2, 5, 8, 3, 4, 7, 2, 5, 9, 8, 5, 4, 2, 1)
 
 freq = dict()
+index = 0
 
-for num in numbers:
-    freq[num] = freq.get(num, 0) + 1
+while index < len(numbers):
+    number = numbers[index]
+    freq[number] = freq.get(number, 0) + 1
+    index += 1
 
 repeated_items = [num
                   for num, count in freq.items()
