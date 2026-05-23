218b62c9b0b13f6831b50c4918ab2ea460845cb4
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-23 19:00:35 +0530

    [IMP] Remove duplicate from tuple using loop with preserve order

--- Phase2/remove_duplicate_from_tuple.py
+++ Phase2/remove_duplicate_from_tuple.py
@@ -2,5 +2,13 @@
 
 numbers = (1, 2, 3, 1, 5, 6, 2, 4, 5, 6, 7, 8, 9, 5)
 
-unique_numbers = tuple(dict.fromkeys(numbers))
-print(f"Unique numbers: {unique_numbers}")
+unique_numbers = []
+seen = set()
+
+for number in numbers:
+    if number not in seen:
+        unique_numbers.append(number)
+        seen.add(number)
+
+result = tuple(unique_numbers)
+print(f"Unique numbers: {result}")
