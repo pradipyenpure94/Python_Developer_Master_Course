1c1633e1e8749f7d243de73a4f24434d7512812d
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-23 19:22:17 +0530

    [IMP] Remove duplicate tuple from tuple using while loop

--- Phase2/remove_duplicate_from_tuple.py
+++ Phase2/remove_duplicate_from_tuple.py
@@ -3,6 +3,15 @@
 numbers = (1, 2, 3, 1, 5, 6, 2, 4, 5, 6, 7, 8, 9, 5)
 
 seen = set()
-unique_numbers = tuple(number for number in numbers if not (
-    number in seen or seen.add(number)))
-print(f"Unique numbers: {unique_numbers}")
+unique_numbers = []
+index = 0
+
+while index < len(numbers):
+    number = numbers[index]
+    if number not in seen:
+        unique_numbers.append(number)
+        seen.add(number)
+    index += 1
+
+result = tuple(unique_numbers)
+print(f"Unique numbers: {result}")
