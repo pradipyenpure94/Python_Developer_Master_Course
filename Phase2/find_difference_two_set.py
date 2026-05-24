5fecbde8051e4698dfe913146bccf963b421a420
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-24 14:37:08 +0530

    [IMP] Find difference between two sets using while loop

--- Phase2/find_difference_two_set.py
+++ Phase2/find_difference_two_set.py
@@ -3,10 +3,14 @@
 prime_numbers = {2, 3, 5, 7, 11, 13, 17, 19}
 odd_numbers = {1, 3, 5, 7, 9, 11, 13}
 
+prime_numbers_list = list(prime_numbers)
+index = 0
 difference = set()
 
-for number in prime_numbers:
+while index < len(prime_numbers_list):
+    number = prime_numbers_list[index]
     if number not in odd_numbers:
         difference.add(number)
+    index += 1
 
 print(f"Difference: {difference}")
