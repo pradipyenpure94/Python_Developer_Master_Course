715d32c08591c33376becdaf4e1ae9d0f80ced6f
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-24 14:18:40 +0530

    [ADD] Add program to find difference between two sets using difference operator

---
+++ Phase2/find_difference_two_set.py
@@ -0,0 +1,7 @@
+"""Find difference between two sets."""
+
+prime_numbers = {2, 3, 5, 7, 11, 13, 17, 19}
+odd_numbers = {1, 3, 5, 7, 9, 11, 13}
+
+difference = prime_numbers - odd_numbers
+print(f"Difference: {difference}")
