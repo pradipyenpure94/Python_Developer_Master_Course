f3314020c1d69c46623dee5282ae169239b46d43
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-24 18:35:21 +0530

    [IMP] Find common list elements betwen two lists using list comprehension

--- Phase2/find_common_elements.py
+++ Phase2/find_common_elements.py
@@ -3,14 +3,7 @@
 prime_numbers = [2, 3, 5, 7]
 odd_numbers = [1, 3, 5, 7]
 
-common_elements = []
 odd_numbers_set = set(odd_numbers)
-index = 0
-
-while index < len(prime_numbers):
-    number = prime_numbers[index]
-    if number in odd_numbers_set:
-        common_elements.append(number)
-    index += 1
-
+common_elements = [number for number in prime_numbers
+                   if number in odd_numbers_set]
 print(f"Common elements: {common_elements}")
