5decd4c3c3e050052d773a0101c5ac4da29f6075
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-24 13:05:53 +0530

    [ADD] Add program to union of two sets

---
+++ Phase2/find_union_of_two_set.py
@@ -0,0 +1,8 @@
+"""Find union of two sets."""
+
+even_numbers = {2, 4, 6, 8, 10}
+odd_numbers = {1, 3, 5, 7, 9}
+
+# union of two sets
+numbers = odd_numbers | even_numbers
+print(f"Union of two sets: {numbers}")
