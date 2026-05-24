38fb7967b572665d0fd21934c21793adea3b203a
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-24 15:53:08 +0530

    [IMP] Manual implementation of checking subset and superset logic using loop

--- Phase2/check_subset_and_superset.py
+++ Phase2/check_subset_and_superset.py
@@ -4,9 +4,21 @@ numbers = {1, 2, 3, 4, 5, 6}
 even_numbers = {2, 4, 6}
 
 # Check subset
-result = even_numbers.issubset(numbers)
+result = True
+
+for number in even_numbers:
+    if number not in numbers:
+        result = False
+        break
+
 print(f"Is subset ? {result}")
 
 # Check superset
-result = numbers.issuperset(even_numbers)
+result = True
+
+for number in even_numbers:
+    if number not in numbers:
+        result = False
+        break
+
 print(f"Is superset? {result}")
