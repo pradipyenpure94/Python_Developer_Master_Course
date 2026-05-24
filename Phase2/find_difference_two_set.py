c50ddf277647a232ed6fa532bb63518f941b0fa3
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-24 14:21:59 +0530

    [IMP] Add program to find difference between two sets using difference()

--- Phase2/find_difference_two_set.py
+++ Phase2/find_difference_two_set.py
@@ -3,5 +3,5 @@
 prime_numbers = {2, 3, 5, 7, 11, 13, 17, 19}
 odd_numbers = {1, 3, 5, 7, 9, 11, 13}
 
-difference = prime_numbers - odd_numbers
+difference = prime_numbers.difference(odd_numbers)
 print(f"Difference: {difference}")
