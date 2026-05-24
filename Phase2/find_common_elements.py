d6180bb6df76ec29cbeef34488bce041303b3746
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-24 18:31:23 +0530

    [IMP] Find common elements between two lists using while loop

--- Phase2/find_common_elements.py
+++ Phase2/find_common_elements.py
@@ -5,9 +5,12 @@ odd_numbers = [1, 3, 5, 7]
 
 common_elements = []
 odd_numbers_set = set(odd_numbers)
+index = 0
 
-for number in prime_numbers:
+while index < len(prime_numbers):
+    number = prime_numbers[index]
     if number in odd_numbers_set:
         common_elements.append(number)
+    index += 1
 
 print(f"Common elements: {common_elements}")
