ac061e2f16dcabc5acf7c3e4011abd587862d729
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-24 19:53:01 +0530

    [IMP] check if two lists have common element using loop

--- Phase2/check_two_list_common_items.py
+++ Phase2/check_two_list_common_items.py
@@ -3,5 +3,11 @@
 list1 = [2, 4, 6, 8, 10]
 list2 = [8, 6, 4, 2]
 
-has_common = not set(list1).isdisjoint(list2)
+has_common = False
+list2_set = set(list2)
+for number in list1:
+    if number in list2_set:
+        has_common = True
+        break
+
 print(f"Has common element? {has_common}")
