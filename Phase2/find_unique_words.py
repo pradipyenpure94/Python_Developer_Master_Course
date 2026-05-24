eef0ac53aeb589284c0225452cdc67b1f3692879
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-24 19:30:22 +0530

    [IMP] Extract unique words from input text using loop

--- Phase2/find_unique_words.py
+++ Phase2/find_unique_words.py
@@ -4,6 +4,11 @@ import string
 
 sentence = "Python is good, so, im learning python"
 
-unique_words = {word.strip(string.punctuation)
-                for word in sentence.casefold().split()}
+words = sentence.casefold().split()
+unique_words = set()
+
+for word in words:
+    clean_word = word.strip(string.punctuation)
+    unique_words.add(clean_word)
+
 print(f"Unique words: {unique_words}")
