feddd36969983ed4adcdf6b1bb6794e74df04de5
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-24 13:12:01 +0530

    [IMP] Add program to union of two sets using add()

--- Phase2/find_union_of_two_set.py
+++ Phase2/find_union_of_two_set.py
@@ -4,5 +4,9 @@ even_numbers = {2, 4, 6, 8, 10}
 odd_numbers = {1, 3, 5, 7, 9}
 
 # union of two sets
-numbers = odd_numbers.union(even_numbers)
+numbers = odd_numbers.copy()
+
+for number in even_numbers:
+    numbers.add(number)
+
 print(f"Union of two sets: {numbers}")
