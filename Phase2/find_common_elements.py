b97ab3ef22ae618f8dcc2425e5dd14e1b50181f1
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-24 18:26:58 +0530

    [IMP] Implement manually, find common elements between two lists using for loop

--- Phase2/find_common_elements.py
+++ Phase2/find_common_elements.py
@@ -3,5 +3,11 @@
 prime_numbers = [2, 3, 5, 7]
 odd_numbers = [1, 3, 5, 7]
 
-common_elements = list(set(prime_numbers).intersection(set(odd_numbers)))
+common_elements = []
+odd_numbers_set = set(odd_numbers)
+
+for number in prime_numbers:
+    if number in odd_numbers_set:
+        common_elements.append(number)
+
 print(f"Common elements: {common_elements}")
