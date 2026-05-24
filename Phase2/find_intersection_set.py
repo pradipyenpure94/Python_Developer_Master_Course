fc5005bb83a232af63e1d9ffcc52b9251de87250
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-24 13:23:27 +0530

    [IMP] Add program to find intersection of two sets using intersection()

--- Phase2/find_intersection_set.py
+++ Phase2/find_intersection_set.py
@@ -3,5 +3,5 @@
 prime_numbers = {2, 3, 5, 7, 11, 13}
 odd_numbers = {1, 3, 5, 7, 9, 11, 13}
 
-common_numbers = prime_numbers & odd_numbers
+common_numbers = prime_numbers.intersection(odd_numbers)
 print(f"Common numbers: {common_numbers}")
