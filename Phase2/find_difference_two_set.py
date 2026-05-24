ad0efd1728ef46d77d1652b8248251abc1f1627f
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-24 14:31:41 +0530

    [IMP] Add program to difference between two sets using loop

--- Phase2/find_difference_two_set.py
+++ Phase2/find_difference_two_set.py
@@ -3,5 +3,10 @@
 prime_numbers = {2, 3, 5, 7, 11, 13, 17, 19}
 odd_numbers = {1, 3, 5, 7, 9, 11, 13}
 
-difference = prime_numbers.difference(odd_numbers)
+difference = set()
+
+for number in prime_numbers:
+    if number not in odd_numbers:
+        difference.add(number)
+
 print(f"Difference: {difference}")
