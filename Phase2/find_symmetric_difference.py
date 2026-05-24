158437c63b3bb2d15e7a1d371e66ecb61757253e
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-24 14:51:48 +0530

    [IMP] Find symmetric difference between two sets using symmetric difference operator

--- Phase2/find_symmetric_difference.py
+++ Phase2/find_symmetric_difference.py
@@ -3,5 +3,5 @@
 numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 even_numbers = {2, 4, 6, 8, 10, 11, 12, 13}
 
-symmetric_difference = numbers.symmetric_difference(even_numbers)
+symmetric_difference = numbers ^ even_numbers
 print(f"Symmetric difference: {symmetric_difference}")
