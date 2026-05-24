c628f674019b16c0fb5c07766c05aa8b1a8663d8
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-24 14:02:49 +0530

    [IMP] Find intersection of two sets using while loop

--- Phase2/find_intersection_set.py
+++ Phase2/find_intersection_set.py
@@ -4,9 +4,13 @@ prime_numbers = {2, 3, 5, 7, 11, 13}
 odd_numbers = {1, 3, 5, 7, 9, 11, 13}
 
 common_numbers = set()
+index = 0
+prime_list_numbers = list(prime_numbers)
 
-for number in odd_numbers:
-    if number in prime_numbers:
+while index < len(prime_numbers):
+    number = prime_list_numbers[index]
+    if number in odd_numbers:
         common_numbers.add(number)
+    index += 1
 
 print(f"Common numbers: {common_numbers}")
