7dc99721a83598def49432d4ff1ce3ca0b0c2514
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-24 19:33:57 +0530

    [IMP] Extract unique words from input text using while loop

--- Phase2/find_unique_words.py
+++ Phase2/find_unique_words.py
@@ -6,9 +6,11 @@ sentence = "Python is good, so, im learning python"
 
 words = sentence.casefold().split()
 unique_words = set()
+index = 0
 
-for word in words:
-    clean_word = word.strip(string.punctuation)
+while index < len(words):
+    clean_word = words[index].strip(string.punctuation)
     unique_words.add(clean_word)
+    index += 1
 
 print(f"Unique words: {unique_words}")
