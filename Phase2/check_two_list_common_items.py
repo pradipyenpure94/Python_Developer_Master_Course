e4514ebde10f4ae42b3c39e1e46e5ac928463fec
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-24 19:58:22 +0530

    [IMP] Check if two lists have common element using while loop

--- Phase2/check_two_list_common_items.py
+++ Phase2/check_two_list_common_items.py
@@ -5,9 +5,14 @@ list2 = [8, 6, 4, 2]
 
 has_common = False
 list2_set = set(list2)
-for number in list1:
+index = 0
+
+while index < len(list1):
+    number = list1[index]
     if number in list2_set:
         has_common = True
         break
+
+    index += 1
 
 print(f"Has common element? {has_common}")
