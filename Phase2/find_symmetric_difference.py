c21902dbac4231be97f3923a78a477d3916f4ef3
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-24 15:00:12 +0530

    [IMP] Find symmetric difference between two sets using left and right diff

--- Phase2/find_symmetric_difference.py
+++ Phase2/find_symmetric_difference.py
@@ -3,5 +3,9 @@
 numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 even_numbers = {2, 4, 6, 8, 10, 11, 12, 13}
 
-symmetric_difference = numbers ^ even_numbers
+left_diff = numbers.difference(even_numbers)
+right_diff = even_numbers.difference(numbers)
+
+symmetric_difference = left_diff.union(right_diff)
+
 print(f"Symmetric difference: {symmetric_difference}")
