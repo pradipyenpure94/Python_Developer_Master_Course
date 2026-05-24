61a0c1a720e2a465c8685e5f6bd82549afd1aee9
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-24 14:48:28 +0530

    [ADD] Add program to find symmetric diff. between two sets using symmetric_difference()

---
+++ Phase2/find_symmetric_difference.py
@@ -0,0 +1,7 @@
+"""Find symmetric difference between two sets."""
+
+numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+even_numbers = {2, 4, 6, 8, 10, 11, 12, 13}
+
+symmetric_difference = numbers.symmetric_difference(even_numbers)
+print(f"Symmetric difference: {symmetric_difference}")
