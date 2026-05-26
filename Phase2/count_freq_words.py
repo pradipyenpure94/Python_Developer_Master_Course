227e6c808caf61e45baf8c4a0517c79f634b1bf6
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-26 19:57:33 +0530

    [IMP] Count frequency of words using while loop and dictionary

--- Phase2/count_freq_words.py
+++ Phase2/count_freq_words.py
@@ -3,11 +3,16 @@ import string
 
 text = "I love python and i am achive goal as python, developer job"
 
-clean_text = "".join(char for char in text if char not in string.punctuation)
+clean_text = "".join(char for char in text.casefold()
+                     if char not in string.punctuation)
 words = clean_text.split()
 word_freq = {}
+index = 0
 
-for word in words:
+while index < len(words):
+    word = words[index]
     word_freq[word] = word_freq.get(word, 0) + 1
+
+    index += 1
 
 print(f"Count word frequency: {word_freq}")
