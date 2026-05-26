279336d49af8b15137c87d78cf1c7441807ae0cf
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-26 15:23:27 +0530

    [IMP] Improve code to find first repeat element usinng while loop

--- Phase2/find_first_repeated_element.py
+++ Phase2/find_first_repeated_element.py
@@ -4,13 +4,19 @@ numbers = [1, 2, 3, 1, 5, 9, 2, 4, 7]
 
 seen = set()
 found = False
+index = 0
 
-for number in numbers:
+while index < len(numbers):
+    number = numbers[index]
+
     if number in seen:
         print(f"First repeated number: {number}")
         found = True
         break
+
     seen.add(number)
+
+    index += 1
 
 if not found:
     print("No repeated element found.")
