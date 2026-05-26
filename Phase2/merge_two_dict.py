eae6434fb81537593914917d25a06da9356899f7
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-26 18:57:39 +0530

    [ADD] Add program to merge two dictionaries using '|' operator

---
+++ Phase2/merge_two_dict.py
@@ -0,0 +1,7 @@
+"""Merge two dictionaries."""
+
+students1 = {"name": "Pradip", "age": 32}
+students2 = {"age": 33, "gender": "Male"}
+
+students = students1 | students2
+print(f"Merge two dictionaries: {students}")
