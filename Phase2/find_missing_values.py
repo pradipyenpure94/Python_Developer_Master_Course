0b65aad59df5b67ac6407b53fd25e30657c963b7
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-26 12:02:33 +0530

    [IMP] Implement, finding missing values using loop

--- Phase2/find_missing_values.py
+++ Phase2/find_missing_values.py
@@ -1,7 +1,12 @@
 """Find missing values using set difference."""
 
 numbers = {1, 2, 3, 5, 7, 8, 9, 10}
-full_numbers = set(range(1, 11))
 
-missing_numbers = full_numbers.difference(numbers)
+full_numbers = set(range(1, 11))
+missing_numbers = set()
+
+for number in full_numbers:
+    if number not in numbers:
+        missing_numbers.add(number)
+
 print(f"Missing numbers: {missing_numbers}")
