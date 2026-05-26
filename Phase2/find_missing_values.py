b46dabfc27ad99c9915a6d493775e8198c9d1203
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-26 12:14:57 +0530

    [IMP] Find missing number values using while loop

--- Phase2/find_missing_values.py
+++ Phase2/find_missing_values.py
@@ -2,11 +2,14 @@
 
 numbers = {1, 2, 3, 5, 7, 8, 9, 10}
 
-full_numbers = set(range(1, 11))
+full_numbers = list(range(1, 11))
 missing_numbers = set()
+index = 0
 
-for number in full_numbers:
+while index < len(full_numbers):
+    number = full_numbers[index]
     if number not in numbers:
         missing_numbers.add(number)
+    index += 1
 
 print(f"Missing numbers: {missing_numbers}")
