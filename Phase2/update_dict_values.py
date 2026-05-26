2a3109c4317317997350a5ee64750c7b510fc666
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-26 18:11:08 +0530

    [ADD] Add program to update dictionary values

---
+++ Phase2/update_dict_values.py
@@ -0,0 +1,7 @@
+"""Update dictionary values."""
+
+students = {"name": "Pradip", "age": 31}
+
+# Update dictionary values
+students.update({"age": 33})
+print(f"Updated students values: {students}")
