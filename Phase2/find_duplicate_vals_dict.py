ffbd2886e153e912f23ebc02072544721fafe5a4
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-28 15:05:29 +0530

    [IMP] Improved code of find duplicate values in dictionary

--- Phase2/find_duplicate_vals_dict.py
+++ Phase2/find_duplicate_vals_dict.py
@@ -4,7 +4,8 @@ data = {"a": 1, "b": 1, "c": 2, "d": 3, "e": 3}
 
 seen = set()
 
-data_values = data.values()
-duplicates = {value for value in data_values
-              if value in seen or seen.add(value)}
+data_values = list(data.values())
+duplicates = {value
+              for value in data_values
+              if data_values.count(value) > 1}
 print(f"Duplicate values in dictionary: {duplicates}")
