380d2a6dbc1f027b34fc084355d9413a1b67fe2c
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-28 15:01:42 +0530

    [IMP] Add program to find duplicate values in dictionary using set comprehension

--- Phase2/find_duplicate_vals_dict.py
+++ Phase2/find_duplicate_vals_dict.py
@@ -3,6 +3,8 @@
 data = {"a": 1, "b": 1, "c": 2, "d": 3, "e": 3}
 
 seen = set()
-duplicates = {value for value in data.values()
+
+data_values = data.values()
+duplicates = {value for value in data_values
               if value in seen or seen.add(value)}
 print(f"Duplicate values in dictionary: {duplicates}")
