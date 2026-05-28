6aaad1e95c73b61a5b0d2a50ad60e75d53b55342
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-28 16:36:31 +0530

    [ADD] Store employee details using nested dictionary

---
+++ Phase2/emp_details_dict.py
@@ -0,0 +1,12 @@
+"""Store employee details using nested dictionary"""
+
+employee_details = {
+    "EMP001":
+        {
+            "name": "Pradip"
+        },
+    "EMP002":
+        {
+            "name": "Amit"
+        }
+    }
