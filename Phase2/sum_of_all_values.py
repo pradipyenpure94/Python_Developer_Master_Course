5bf70ad4dd5d9d24380f21d0cd582155e0866100
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-28 12:57:42 +0530

    [ADD] Add program to sum of all dict values using sum()

---
+++ Phase2/sum_of_all_values.py
@@ -0,0 +1,7 @@
+"""Sum of all dictionary values."""
+
+students = {"Ajay": 100, "amit": 45}
+
+# Sum of all dictionary values
+total = sum(students.values())
+print(f"Sum of all dictionary values: {total}")
