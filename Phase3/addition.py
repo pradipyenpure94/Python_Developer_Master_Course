af99b22aaea761369bfb8eca7bb2003a822625b8
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-29 15:42:48 +0530

    [ADD] Add program to addition of two numbers

---
+++ Phase3/addition.py
@@ -0,0 +1,21 @@
+"""Addition using function."""
+
+
+def add_numbers(num1: int, num2: int) -> int:
+    """Return the addition of two numbers.
+    Args:
+        num1 (int): First input number
+        num2 (int): Second input number
+    Returns:
+        int: Addition of two numbers.
+    """
+    return num1 + num2
+
+
+if __name__ == "__main__":
+    try:
+        first_number = int(input("Enter first number: "))
+        second_number = int(input("Enter second number: "))
+        print(f"Addition: {add_numbers(num1=first_number, num2=second_number)}")
+    except ValueError:
+        print("Invalid input! Please enter a valid input.")
