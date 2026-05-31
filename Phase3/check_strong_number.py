ee741bfae9b39ab4b627f3f71f220b2a6246e40e
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-31 19:38:52 +0530

    [ADD] Add program to check whether number is a strong number or not using factorial()

---
+++ Phase3/check_strong_number.py
@@ -0,0 +1,41 @@
+"""Check whether number is strong or not."""
+
+from math import factorial
+
+
+def factorial_sum(num: int) -> int:
+    """Return the factorial sum.
+
+    Args:
+        num (int): Input number.
+
+    Returns:
+        int: Sum of factorial of each digit in number.
+    """
+    return sum(factorial(int(i)) for i in str(num))
+
+
+def is_strong_number(num: int) -> bool:
+    """
+    Check whether number is a strong number or not.
+
+    Args:
+        num (int): Input number.
+
+    Returns:
+        bool: True if the number is a strong number, otherwise False.
+    """
+    if num <= 0:
+        raise ValueError("Number must be greater than zero.")
+    return num == factorial_sum(num=num)
+
+
+if __name__ == "__main__":
+    try:
+        number = int(input("Enter a number: "))
+        if is_strong_number(num=number):
+            print(f"{number} is a strong number.")
+        else:
+            print(f"{number} is not a strong number")
+    except ValueError as error:
+        print(f"Error: {error}")
