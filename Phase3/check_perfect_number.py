81862250feb64ce83777852c4c22b4b3ebc19732
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-05-31 18:39:03 +0530

    [ADD] Add program to check whether number is a perfect number or not

---
+++ Phase3/check_perfect_number.py
@@ -0,0 +1,33 @@
+"""Check whether number is a perfect number or not."""
+
+
+def is_perfect_number(num: int) -> bool:
+    """
+    Check whether number is a perfect number or not.
+
+    Args:
+        num (int): Input number.
+
+    Returns:
+        bool: True if number is a perfect number otherwise False.
+    """
+    if num <= 0:
+        raise ValueError("Number must be greater than zero.")
+
+    total = 0
+
+    for i in range(1, num):
+        if num % i == 0:
+            total += i
+    return num == total
+
+
+if __name__ == "__main__":
+    try:
+        number = int(input("Enter a number: "))
+        if is_perfect_number(num=number):
+            print(f"{number} is a perfect number.")
+        else:
+            print(f"{number} is not a perfect number.")
+    except ValueError as error:
+        print(f"Error: {error}")
