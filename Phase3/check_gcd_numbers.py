53436e160c5cbe9bc639516de2c7f913935e1542
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-06-01 15:34:59 +0530

    [ADD] Add program to find gcd of two numbers using euclidean algoritham

---
+++ Phase3/check_gcd_numbers.py
@@ -0,0 +1,30 @@
+"""Check GCD."""
+
+
+def find_gcd(num1: int, num2: int) -> int:
+    """
+    Return the GCD of two numbers.
+
+    Args:
+        num1 (int): First input number.
+        num2 (int): Second input number.
+
+    Returns:
+        int: Greatest common divisor number.
+    """
+    num1 = abs(num1)
+    num2 = abs(num2)
+
+    while num2 != 0:
+        num1, num2 = num2, num1 % num2
+    return num1
+
+
+if __name__ == "__main__":
+    try:
+        first_number = int(input("Enter first number: "))
+        second_number = int(input("Enter second number: "))
+        result = find_gcd(num1=first_number, num2=second_number)
+        print(f"GCD of {first_number} and {second_number} is: {result}")
+    except ValueError:
+        print("Invalid input! Please enter an integer.")
