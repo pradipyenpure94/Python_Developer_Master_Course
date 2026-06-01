9f3b72f9e2c549d3bae7bddcea310eb2bdc47c48
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-06-01 18:30:37 +0530

    [ADD] Add program to convert binary to decimal conversion

---
+++ Phase3/binary_to_decimal.py
@@ -0,0 +1,21 @@
+"""Binary to decimal."""
+
+
+def convert_binary_to_decimal(num: str) -> int:
+    """Return the binary to decimal number.
+
+    Args:
+        num (int): Input binary number.
+
+    Returns:
+        int: Decimal number.
+    """
+    if any(number not in {"0", "1"} for number in num):
+        raise ValueError("Binary number must contain 0 or 1")
+    return int(num, 2)
+
+
+if __name__ == "__main__":
+    number = input("Enter binary number: ")
+    result = convert_binary_to_decimal(num=number)
+    print(f"Binary to Decimal conversion: {result}")
