3d09db1731e994c201e9008c234b8a609bc1851a
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-06-02 15:24:03 +0530

    [ADD] Add program to count special characters in string using sum()

---
+++ Phase3/count_special_chars.py
@@ -0,0 +1,20 @@
+"""Count special characters."""
+
+
+def count_special_characters(text: str) -> int:
+    """
+    Return the special characters count in string.
+
+    Args:
+        text (str): Input text.
+
+    Returns:
+        int: Count of special characters in string.
+    """
+    return sum(1 for char in text if not char.isalnum() and not char.isspace())
+
+
+if __name__ == "__main__":
+    input_text = input("Enter a string: ")
+    result = count_special_characters(text=input_text)
+    print(f"Count of special characters: {result}")
