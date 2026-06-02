ca6943769d451c0532733a311111ccf42813ae74
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-06-02 14:16:41 +0530

    [ADD] Add program to count lowercase letters using sum()

---
+++ Phase3/count_lowercase_letters.py
@@ -0,0 +1,20 @@
+"""Count lowercase letters."""
+
+
+def count_lowercase_letters(text: str) -> int:
+    """
+    Return the count of lowercase letters in string.
+
+    Args:
+        text (str): Input text.
+
+    Returns:
+        int: Count of lowercase letters in string.
+    """
+    return sum(1 for char in text if char.isalpha() and char.islower())
+
+
+if __name__ == "__main__":
+    input_text = input("Enter a string: ")
+    result = count_lowercase_letters(text=input_text)
+    print(f"Count lowercase letters: {result}")
