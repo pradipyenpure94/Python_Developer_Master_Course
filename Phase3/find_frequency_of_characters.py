a81ed12132ab07c2fd7108884ba2539661161b9d
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-06-02 17:18:20 +0530

    [ADD] Add program to count frequency of characters in string

--- Phase3/find_frequency_of_characters.py
+++ Phase3/find_frequency_of_characters.py
@@ -3,17 +3,24 @@
 
 def find_frequency_of_characters(text: str) -> dict[str, int]:
     """
-    Return the frequency of characters in string.
+    Return the frequency of each character in string.
 
     Args:
         text (str): Input text.
 
     Returns:
-        dict[str, int]: Frequency of characters.
+        dict[str, int]: Mapping of characters to thier frequencies.
     """
     freq = {}
-    for char in text.casefold():
+    index = 0
+    input_text = text.casefold()
+
+    while index < len(input_text):
+        char = input_text[index]
         freq[char] = freq.get(char, 0) + 1
+
+        index += 1
+
     return freq
 
 
