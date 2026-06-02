c98fe62c3d9308c20161b887486eaaed43c437e9
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-06-02 17:39:35 +0530

    [ADD] Add program to replace vowels by star in string using .join()

--- Phase3/replace_vowel_with_astrick.py
+++ Phase3/replace_vowel_with_astrick.py
@@ -11,8 +11,8 @@ def replace_vowel_by_star(text: str) -> str:
     Returns:
         str: Replaced vowel by *
     """
-    translation_table = str.maketrans("AEIOUaeiou", "**********")
-    return text.translate(translation_table)
+    vowels = "AEIOUaeiou"
+    return "".join("*" if char in vowels else char for char in text)
 
 
 if __name__ == "__main__":
