ddddf789fdecda4d294313d831682f4f4c81dfc5
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-06-03 12:24:19 +0530

    [ADD] Add program to sum of list elements using sum()

---
+++ Phase3/sum_of_list_elements.py
@@ -0,0 +1,20 @@
+"""Find sum of list elements."""
+
+
+def sum_of_list_numbers(nums: list[int]) -> int:
+    """
+    Return the sum of list elements.
+
+    Args:
+        nums (list[int]): Input numbers list.
+
+    Returns:
+        int: sum of list elements.
+    """
+    return sum(number for number in nums)
+
+
+if __name__ == "__main__":
+    numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    result = sum_of_list_numbers(nums=numbers)
+    print(f"Sum of List elements: {result}")
