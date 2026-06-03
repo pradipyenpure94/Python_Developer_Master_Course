6bb4aa584571d9c0b0b491037250a69b876d2726
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-06-03 13:54:31 +0530

    [ADD] Add program to find second largest number from list

---
+++ Phase3/find_second_largest_number.py
@@ -0,0 +1,37 @@
+"""Find second largest number."""
+
+
+def find_second_largest_number(nums: list[int]) -> int:
+    """
+    Return the second largest number from list
+
+    Args:
+        nums (list[int]): Input numbers list.
+
+    Returns:
+        int: Second largest number from list.
+
+    Raises:
+        ValueError: If the list does not contain at least two distinct numbers.
+    """
+    if len(set(nums)) < 2:
+        raise ValueError(
+            "Need at least two distinct numbers to find second largest.")
+
+    largest_num = second_largest_num = float('-inf')
+
+    for number in nums:
+        if number > largest_num:
+            second_largest_num = largest_num
+            largest_num = number
+
+        elif number > second_largest_num:
+            second_largest_num = number
+
+    return second_largest_num
+
+
+if __name__ == "__main__":
+    numbers = [4, 1, 5, 10, 1]
+    result = find_second_largest_number(nums=numbers)
+    print(f"Second Largest number: {result}")
