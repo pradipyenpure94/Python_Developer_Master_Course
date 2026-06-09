a5b93b2e18c561a18cd8bbbca6005de6f7ef7a08
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-06-09 18:57:12 +0530

    [ADD] Find max element in the list using reduce() and built-in max()

--- Phase3/find_max_element.py
+++ Phase3/find_max_element.py
@@ -11,14 +11,15 @@ def find_maximum_element(nums: list[int]) -> int | None:
         nums (list[int]): Input element list.
 
     Returns:
-        int | None: Maximum element in the list, or None if the list empty.
+        int | None: Maximum element in the list, or None if the list is empty.
     """
     if not nums:
         return None
-    return reduce(lambda x, y: x if x > y else y, nums)
+    return reduce(max, nums)
 
 
 if __name__ == "__main__":
     numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    numbers = []
     result = find_maximum_element(nums=numbers)
     print(f"Maximum element: {result}")
