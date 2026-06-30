1e4c11b8c4657a7bde25443e72126c7142cc10d2
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-06-30 18:26:21 +0530

    [ADD] Merge two sorted lists using bubble sort

--- Part1/merge_two_sorted_lists.py
+++ Part1/merge_two_sorted_lists.py
@@ -1,9 +1,9 @@
 """Merge two sorted lists."""
 
 
-def merge_list(list1: list[int], list2: list[int]) -> list[int]:
+def merge_sorted_lists(list1: list[int], list2: list[int]) -> list[int]:
     """
-    Return a merge sorted lists.
+    Return a merged sorted list.
 
     Args:
         list1 (list[int]): First input list.
@@ -12,11 +12,24 @@ def merge_list(list1: list[int], list2: list[int]) -> list[int]:
     Returns:
         list[int]: A merged sorted list.
     """
-    return sorted(list1 + list2)
+    merged_list = list1 + list2
+    n = len(merged_list)
+
+    for i in range(n):
+        found = False
+        for j in range(n -i -1):
+            if merged_list[j] > merged_list[j + 1]:
+                merged_list[j], merged_list[j + 1] = (merged_list[j + 1],
+                                                      merged_list[j])
+                found = True
+
+        if not found:
+            break
+    return merged_list
 
 
 if __name__ == "__main__":
     first_input_data = [2, 4, 6, 8]
     second_input_data = [1, 3, 5, 7]
-    result = merge_list(list1=first_input_data, list2=second_input_data)
+    result = merge_sorted_lists(list1=first_input_data, list2=second_input_data)
     print(f"Merged List: {result}")
