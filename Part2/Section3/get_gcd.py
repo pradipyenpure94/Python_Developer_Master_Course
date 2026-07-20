4479cac2e37b1360d26dc94dc61b2f15a9108865
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-07-20 19:12:39 +0530

    [IMP] Calculate GCD using euclidean algorithm

--- Part2/Section3/get_gcd.py
+++ Part2/Section3/get_gcd.py
@@ -1,7 +1,5 @@
 """GCD."""
 
-from math import gcd
-
 # As per the business requirement, Defined the limits.
 MIN_NUMBER = 1
 MAX_NUMBER = 9999
@@ -11,8 +9,12 @@ def validate_number(number: int, field_name: str) -> None:
     """
     Validate the number.
 
+    Args:
+        number (int): Input number.
+        field_name (str): Input field name.
+
     Raises:
-        ValueError: If the number is outside range. its not allowed.
+        ValueError: If the number is outside the allowed range.
     """
     if not MIN_NUMBER <= number <= MAX_NUMBER:
         raise ValueError(
@@ -22,16 +24,21 @@ def validate_number(number: int, field_name: str) -> None:
 
 def calculate_gcd(first_number: int, second_number: int) -> int:
     """
-    Return a GCD number.
+    Return the greatest common divisor of two input numbers.
 
     Args:
         first_number (int): First input number.
         second_number (int): Second input number.
 
     Returns:
-        int: GCD number.
+        int: The greatest common divisor of the two input numbers.
     """
-    return gcd(first_number, second_number)
+    while second_number != 0:
+        first_number, second_number = (
+            second_number,
+            first_number % second_number
+        )
+    return first_number
 
 
 def main() -> None:
