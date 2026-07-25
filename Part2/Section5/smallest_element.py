3d38746ccfff93784ab7d04c5726d11faab3921c
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-07-25 19:58:43 +0530

    [ADD] Find the smallest element in the input numbers list

---
+++ Part2/Section5/smallest_element.py
@@ -0,0 +1,35 @@
+"""Smallest element."""
+
+from largest_element import validate_numbers_list
+
+
+def find_smallest_element(numbers: list[int | float]) -> int | float:
+    """
+    Return the smallest element in the input numbers list.
+
+    Args:
+        numbers (list[int | float]): User input numbers list.
+
+    Returns:
+        int | float: The smallest element in the input numbers list.
+    """
+    return min(numbers)
+
+
+def main() -> None:
+    """Run the Main Program."""
+    try:
+        numbers = [10, 20, 40, 15, 30, 2, 5]
+        validate_numbers_list(numbers=numbers)
+
+    except ValueError as error:
+        print(f"Error: {error}")
+    except KeyboardInterrupt:
+        print("\nOperation cancelled by the user.")
+    else:
+        result = find_smallest_element(numbers=numbers)
+        print(f"Smallest element: {result}")
+
+
+if __name__ == "__main__":
+    main()
