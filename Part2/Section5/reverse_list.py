9614e1d928073271ab121ed0718ababe69f718f1
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-07-27 16:48:15 +0530

    [ADD] Add list reversal using reversed()

---
+++ Part2/Section5/reverse_list.py
@@ -0,0 +1,34 @@
+"""Reverse list."""
+
+from smallest_element import validate_numbers_list
+
+
+def reverse_list(numbers: list[int | float]) -> list[int | float]:
+    """
+    Return the reversed input list.
+
+    Args:
+        numbers (list[int | float]): Input numbers list.
+
+    Returns:
+        list[int | float]: The reversed list.
+    """
+    return list(reversed(numbers))
+
+
+def main() -> None:
+    """Run the Main Program."""
+    try:
+        numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        validate_numbers_list(numbers=numbers)
+    except ValueError as error:
+        print(f"Error: {error}")
+    except KeyboardInterrupt:
+        print("\nOperation cancelled by the user.")
+    else:
+        result = reverse_list(numbers=numbers)
+        print(f"Reversed List: {result}")
+
+
+if __name__ == "__main__":
+    main()
