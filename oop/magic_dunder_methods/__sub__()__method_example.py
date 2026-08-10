d56e6c448891889fb08315a0a6e30d3c01dea60b
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-08-10 17:32:43 +0530

    [ADD] Implement the __sub__() method

---
+++ oop/magic_dunder_methods/__sub__()__method_example.py
@@ -0,0 +1,27 @@
+"""__sub__() method."""
+
+
+class Employee:
+    """Represent an employee."""
+    def __init__(self, name: str, salary: float) -> None:
+        self.name = name
+        self.salary = salary
+
+    def __sub__(self, other: object) -> tuple[str, float]:
+        if not isinstance(other, Employee):
+            return NotImplemented
+        return (
+            self.name + "-" + other.name,
+            self.salary - other.salary
+        )
+
+
+def main() -> None:
+    """Run the main program."""
+    emp_obj1 = Employee(name="Pradip", salary=125000)
+    emp_obj2 = Employee(name="Amit", salary=30000)
+    print(emp_obj1 - emp_obj2)
+
+
+if __name__ == "__main__":
+    main()
