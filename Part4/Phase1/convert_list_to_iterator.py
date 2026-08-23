1215b526dfc963ca9cd754002b30c274df38184d
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-08-23 15:32:19 +0530

    [IMP] Develop a custom  user define list iterator

--- Part4/Phase1/convert_list_to_iterator.py
+++ Part4/Phase1/convert_list_to_iterator.py
@@ -1,12 +1,41 @@
-"""Program 1: Convert a List into an Iterator."""
+"""Program 1: Convert a List into an Iterator.WAP to User Define iterator."""
 
-numbers = [10, 20, 30, 40, 50]
+from collections.abc import Iterator
+from typing import TypeVar
 
-iterator = iter(numbers)
+T = TypeVar("T")
 
-print(next(iterator))
-print(next(iterator))
-print(next(iterator))
-print(next(iterator))
-print(next(iterator))
-print(next(iterator, "No Value"))
+
+class ListIterator(Iterator[T]):
+    """Represent a list iterator."""
+
+    def __init__(self, data_list: list[T]) -> None:
+        self.data_list = data_list
+        self.index = 0
+
+    def __iter__(self) -> "ListIterator":
+        return self
+
+    def __next__(self) -> T:
+        if self.index < len(self.data_list):
+            item = self.data_list[self.index]
+            self.index += 1
+            return item
+
+        raise StopIteration
+
+
+def main() -> None:
+    """Run the main program."""
+    numbers = [10, 20, 30]
+
+    iterator = ListIterator(data_list=numbers)
+
+    print(next(iterator))
+    print(next(iterator))
+    print(next(iterator))
+    print(next(iterator, "No Value"))
+
+
+if __name__ == "__main__":
+    main()
