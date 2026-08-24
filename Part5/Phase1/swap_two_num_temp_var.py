cbe0ff7afdc90948b540b3488b0e23d45d302776
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-08-24 16:20:52 +0530

    [ADD] Swap two numbers using temp variable

---
+++ Part5/Phase1/swap_two_num_temp_var.py
@@ -0,0 +1,12 @@
+"""Swap two numbers using a temporary variable."""
+
+
+first_number = 10
+second_number = 20
+
+temp = first_number
+first_number = second_number
+second_number = temp
+
+print(f"First number: {first_number}")
+print(f"Second number: {second_number}")
