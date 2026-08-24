2757394ae4793f4bc0aed76dce24b1aa4dc7d899
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-08-24 16:45:58 +0530

    [ADD] Swap two numbers without using third variable

---
+++ Part5/Phase1/swap_numbers.py
@@ -0,0 +1,8 @@
+"""Swap two numbers without using a temporary variable."""
+
+first_number = 10
+second_number = 20
+
+first_number, second_number = second_number, first_number
+print(f"First number  : {first_number}")
+print(f"Second number : {second_number}")
