cd223ebb93cefef83dfede588c4ac2f243f36583
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-08-25 16:52:03 +0530

    [ADD] Calculate the average of numbers

---
+++ Part5/Phase1/avg_numbers.py
@@ -0,0 +1,13 @@
+"""Calculate average of three numbers."""
+
+
+TOTAL_NUMBERS = 3
+
+first_number = 10
+second_number = 20
+third_number = 30
+
+average_of_numbers = (
+    first_number + second_number + third_number
+) / TOTAL_NUMBERS
+print(f"Average of numbers: {average_of_numbers:.2f}")
