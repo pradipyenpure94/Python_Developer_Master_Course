1c2be0220e22081cb08b05b1b570e16629ac9fe0
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-08-26 16:21:08 +0530

    [ADD] Check the different value and its data type

---
+++ Part5/Phase1/check_data_type_val.py
@@ -0,0 +1,13 @@
+"""Check the data type of different python values."""
+
+value1 = 12
+print(f"Value1 data type: {type(value1)}")
+
+value2 = 12.5
+print(f"Value2 data type: {type(value2)}")
+
+value3 = "sai123"
+print(f"Value3 data type: {type(value3)}")
+
+value4 = False
+print(f"Value4 data type: {type(value4)}")
