627049f9b85ea2d2c0df5c1482ad77972c1f6b5c
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-08-26 17:23:59 +0530

    [ADD] Demo of comparison operators

---
+++ Part5/Phase1/comparision_operators.py
@@ -0,0 +1,12 @@
+"""Demonstrate the comparison operators."""
+
+
+first_number = 10
+second_number = 25
+
+print(f"==    : {first_number == second_number}")
+print(f"!=    : {first_number != second_number}")
+print(f"<     : {first_number < second_number}")
+print(f">     : {first_number > second_number}")
+print(f"<=    : {first_number <= second_number}")
+print(f">=    : {first_number >= second_number}")
