d43ff603e7980b16fe52a45c35078740f1241b99
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-08-26 18:54:13 +0530

    [ADD] Demonstrate the bitwise operators

---
+++ Part5/Phase1/bitwise_operators.py
@@ -0,0 +1,16 @@
+"""Demonstrate the bitwise AND OR XOR operators."""
+
+num1 = 10
+num2 = 6
+
+print("Bitwise AND:")
+result = num1 & num2
+print(result)
+
+print("Bitwise OR:")
+result = num1 | num2
+print(result)
+
+print("Bitwise XOR:")
+result = num1 ^ num2
+print(result)
