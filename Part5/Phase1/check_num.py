ff1b3697073a46ec29535ba9727f7b7f495f5ace
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-08-26 19:11:03 +0530

    [ADD]Check whether a number is divisible by both 3 and 5

---
+++ Part5/Phase1/check_num.py
@@ -0,0 +1,8 @@
+"""Check whether a number is divisible by both 3 and 5."""
+
+number = 55
+
+if number % 3 == 0 and number % 5 == 0:
+    print(f"{number} is divisible by 3 and 5.")
+else:
+    print(f"{number} is not divisible by 3 and 5.")
