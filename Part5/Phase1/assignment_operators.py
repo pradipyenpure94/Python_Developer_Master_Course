f2123d27a1b69f14cdd301d95785fca53d7e4a7d
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-08-26 17:56:23 +0530

    [ADD] Demonstrate the assignment operators

---
+++ Part5/Phase1/assignment_operators.py
@@ -0,0 +1,25 @@
+"""Demonstrate the assignment operators."""
+
+value = 10
+print(f"Equal to Assign         : {value}")
+
+value += 10
+print(f"Addition and Assign     : {value}")
+
+value -= 5
+print(f"Subtraction and Assign  : {value}")
+
+value *= 4
+print(f"Multiply and Assign     : {value}")
+
+value /= 2
+print(f"Division and Assign     : {value}")
+
+value //= 4
+print(f"Floor Divide and Assign : {value}")
+
+value %= 4
+print(f"Modulus and Assign      : {value}")
+
+value **= 2
+print(f"Exponent and Assign     : {value}")
