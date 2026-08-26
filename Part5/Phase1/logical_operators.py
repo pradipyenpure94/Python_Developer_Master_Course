97c34b0dadeecbea1e78ae518866cf02ca1ab781
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-08-26 17:37:11 +0530

    [ADD] Demonstrate the logical operators

---
+++ Part5/Phase1/logical_operators.py
@@ -0,0 +1,18 @@
+"""Demonstrate the logical operators."""
+
+# Logical AND:
+print(f"Example of Logical AND: {10 > 3 and 10 == 4}")
+print(f"Example of Logical AND: {10 > 3 and 10 == 10}")
+print("-" * 40)
+print()
+
+# Logical OR:
+print(f"Logical OR: {10 > 3 or 10 == 4}")
+print(f"Logical OR: {10 > 13 or 10 == 10}")
+print(f"Logical OR: {10 > 13 or 10 == 4}")
+print()
+
+# Logical NOT
+print(f"Logical NOT: {not True}")
+print(f"Logical NOT: {not False}")
+print(f"Logical NOT: {not (10==0)}")
