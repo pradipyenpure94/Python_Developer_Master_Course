4b67eb34e671831fe110e2b67e64cd41addbfe2e
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-08-27 17:47:00 +0530

    [ADD] Sum of odd numbers

---
+++ Part5/Phase1/sum_of_odd_numbers.py
@@ -0,0 +1,14 @@
+"""Find the sum of odd numbers from 1 to N."""
+
+try:
+    number = int(input("Enter the number: "))
+    if number < 0:
+        raise ValueError("Number cannot be negative.")
+
+except ValueError as error:
+    print(f"Error: {error}")
+except KeyboardInterrupt:
+    print("\nOperation cancelled by the user.")
+else:
+    sum_odd_numbers = ((number + 1) // 2) ** 2
+    print(f"Sum of Odd numbers from 1 to {number}: {sum_odd_numbers}")
