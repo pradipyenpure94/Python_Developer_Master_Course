9657551439571c19ecc177a644beb7c4e3530076
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-08-27 18:32:58 +0530

    [IMP] Improved code using while loop

--- Part5/Phase1/print_multiplication_tables.py
+++ Part5/Phase1/print_multiplication_tables.py
@@ -1,8 +1,13 @@
 """Print multiplication tables from 1 to 10."""
 
-for number in range(1, 11):
-    table = f"Table of {number}"
+index = 1
+while index <= 10:
+    j = 1
+    table = f"Table of {index}"
     print(table.center(40, "-"))
-    for i in range(1, 11):
-        print(f"{number} x {i} = {number * i}")
+
+    while j <= 10:
+        print(f"{j} x {index} = {j * index}")
+        j += 1
+    index += 1
     print("-" * 40)
