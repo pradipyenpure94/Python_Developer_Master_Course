cac3b25ed0681379d925c8756c248191487c1b99
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-08-28 21:20:40 +0530

    [ADD] Find the max. number in list using while loop

--- Part5/Phase2/find_max_element.py
+++ Part5/Phase2/find_max_element.py
@@ -3,9 +3,10 @@
 
 numbers = [1, 2, 3, 4, 5]
 maximum_number = numbers[0]
+index = 0
 
-for number in numbers:
-    if maximum_number < number:
-        maximum_number = number
-
+while index < len(numbers):
+    if maximum_number < numbers[index]:
+        maximum_number = numbers[index]
+    index += 1
 print(f"Maximum element: {maximum_number}")
