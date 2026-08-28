4d3f0d289b21ed693e045dde4b9c0a5448ec4508
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-08-28 21:17:18 +0530

    [ADD] Find max. element in the list

--- Part5/Phase2/find_max_element.py
+++ Part5/Phase2/find_max_element.py
@@ -2,4 +2,10 @@
 
 
 numbers = [1, 2, 3, 4, 5]
-print(f"Maximum element: {max(numbers)}")
+maximum_number = numbers[0]
+
+for number in numbers:
+    if maximum_number < number:
+        maximum_number = number
+
+print(f"Maximum element: {maximum_number}")
