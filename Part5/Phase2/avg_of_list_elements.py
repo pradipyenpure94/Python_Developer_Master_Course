b21d4cb1e0e2ad7667e34ce84a021a0fdf420c5a
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-08-29 13:37:10 +0530

    [ADD] Find the avg. of list elements using len() and sum()

---
+++ Part5/Phase2/avg_of_list_elements.py
@@ -0,0 +1,10 @@
+"""Find average of list elements."""
+
+
+numbers = [1, 2, 3, 4, 5]
+
+total_items = len(numbers)
+sum_of_list_items = sum(numbers)
+
+average = sum_of_list_items / total_items
+print(f"Average of list items: {average:.2f}")
