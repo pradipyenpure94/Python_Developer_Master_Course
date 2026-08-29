d47bdb526ed94fc9d9c5ba4b684c2ac3e5d13ede
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-08-29 15:45:13 +0530

    [IMP] Find duplicate items using list comprehension and set

--- Part5/Phase2/find_duplicate_elements.py
+++ Part5/Phase2/find_duplicate_elements.py
@@ -3,15 +3,11 @@
 
 companies = ["TCS", "Tech Mahindra", "Odoo", "TCS", "Accenture", "IBM", "Odoo"]
 seen = set()
-duplicate_companies_list = []
-n = len(companies)
-index = 0
 
-while index < n:
-    if companies[index] not in seen:
-        seen.add(companies[index])
-    else:
-        duplicate_companies_list.append(companies[index])
-    index += 1
+duplicate_companies_list = [
+    company for company in companies if (
+        company in seen or seen.add(company)
+    )
+]
 
 print(f"Duplicate companies: {duplicate_companies_list}")
