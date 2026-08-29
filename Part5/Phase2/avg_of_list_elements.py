9ddb06b05363105209590465e9bb95124f9ebb33
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-08-29 13:48:34 +0530

    [IMP] Improved code using while loop and len()

--- Part5/Phase2/avg_of_list_elements.py
+++ Part5/Phase2/avg_of_list_elements.py
@@ -3,12 +3,13 @@
 
 numbers = [1, 2, 3, 4, 5]
 
-count_elements = 0
+count_elements = len(numbers)
 total_of_elements = 0
+index = 0
 
-for number in numbers:
-    count_elements += 1
-    total_of_elements += number
+while index < count_elements:
+    total_of_elements += numbers[index]
+    index += 1
 
 average = total_of_elements / count_elements
 print(f"Average: {average:.2f}")
