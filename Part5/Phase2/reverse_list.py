f8bf70b19a70e0a873b97d45866a1ba08b0227d5
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-08-29 14:34:41 +0530

    [IMP] Reversed list using list comprehension

--- Part5/Phase2/reverse_list.py
+++ Part5/Phase2/reverse_list.py
@@ -2,9 +2,6 @@
 
 numbers = [1, 2, 3, 4, 5]
 n = len(numbers)
-result = []
-
-for index in range(n - 1, -1, -1):
-    result.append(numbers[index])
+result = [numbers[index] for index in range(n - 1, -1, -1)]
 
 print(f"Reversed List: {result}")
