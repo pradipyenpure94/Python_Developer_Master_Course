26b363b42c7c7be31be1778f41f02e3394b6001a
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-08-29 14:05:24 +0530

    [IMP] Count occurrence of element using while loop

--- Part5/Phase2/count_occurrences_of_element.py
+++ Part5/Phase2/count_occurrences_of_element.py
@@ -4,9 +4,9 @@
 numbers = [1, 9, 9, 3]
 target = 9
 count_occurrences = 0  # How many times element appeared.
+index = 0
 
-for number in numbers:
-    if target == number:
-        count_occurrences += 1
-
+while index < len(numbers):
+    count_occurrences += 1 if target == numbers[index] else 0
+    index += 1
 print(f"Result: {count_occurrences}")
