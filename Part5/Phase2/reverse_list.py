ed24c1d93909a624e09e5c9446594f4fc7323b3c
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-08-29 14:40:13 +0530

    [IMP] Reversed list using while loop

--- Part5/Phase2/reverse_list.py
+++ Part5/Phase2/reverse_list.py
@@ -1,7 +1,12 @@
 """Reverse a list."""
 
 numbers = [1, 2, 3, 4, 5]
-n = len(numbers)
-result = [numbers[index] for index in range(n - 1, -1, -1)]
+n = len(numbers) - 1
+result = []
+index = 0
+
+while n >= index:
+    result.append(numbers[n])
+    n -= 1
 
 print(f"Reversed List: {result}")
