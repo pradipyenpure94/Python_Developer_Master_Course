a44b1d19fedf1d4d4c8e15b9617ba2ee85430b98
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-08-30 15:56:41 +0530

    [ADD] Find maximum difference between two elements

---
+++ Part5/Phase2/find_max_diff.py
@@ -0,0 +1,5 @@
+"""Find maximum difference between two elements."""
+
+numbers = [11, 2, 3, 4, 5, 0, 14]
+max_difference = max(numbers) - min(numbers)
+print(f"Maximum difference between elements: {max_difference}")
