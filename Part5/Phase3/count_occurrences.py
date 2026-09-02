d7e10a2f9f950cf8cf3435541f11e29f4abc3b7d
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-09-02 14:52:11 +0530

    [ADD] Count occurrences in my tuple using sum()

--- Part5/Phase3/count_occurrences.py
+++ Part5/Phase3/count_occurrences.py
@@ -1,6 +1,7 @@
 """count occurrences in tuple."""
 
 my_tuple = (1, 9, 9, 3)
-count = my_tuple.count(9)
+target = 9
+count = sum(1 for number in my_tuple if number == target)
 
 print(f"Count occurrences: {count}")
