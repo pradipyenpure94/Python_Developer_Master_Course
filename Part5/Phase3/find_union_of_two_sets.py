0dd66cf1832e5984586ef47bf243293eed378275
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-09-04 13:52:24 +0530

    [ADD] Find union of two sets.

---
+++ Part5/Phase3/find_union_of_two_sets.py
@@ -0,0 +1,8 @@
+"""Find union of two sets."""
+
+
+prime_numbers = {2, 3, 5, 7, 11}
+odd_numbers = {1, 3, 5, 7, 9, 11}
+
+union_of_sets = prime_numbers.union(odd_numbers)
+print(f"Union of sets: {union_of_sets}")
