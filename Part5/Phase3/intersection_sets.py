683afe118890b119e65a7ad313f7cb1c0af422c6
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-09-04 13:58:30 +0530

    [ADD] Find intersection of two sets

---
+++ Part5/Phase3/intersection_sets.py
@@ -0,0 +1,8 @@
+"""Find intersection of two sets."""
+
+
+prime_numbers = {2, 3, 5, 7, 11, 13}
+odd_numbers = {1, 3, 5, 7, 9, 11, 13}
+
+intersection_of_sets = prime_numbers.intersection(odd_numbers)
+print(f"Intersection of sets: {intersection_of_sets}")
