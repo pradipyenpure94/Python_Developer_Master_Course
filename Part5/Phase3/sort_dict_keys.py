c629fc597fb775812adb6b7ee1e3642fd844b83b
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-09-04 17:08:25 +0530

    [ADD] Sort dict by keys

---
+++ Part5/Phase3/sort_dict_keys.py
@@ -0,0 +1,6 @@
+"""Sort dictionary by keys."""
+
+my_dictionary = {"apple": 30, "cherry": 25, "banana": 29}
+
+result = dict(sorted(my_dictionary.items()))
+print(f"Sorted dictionary by keys: {result}")
