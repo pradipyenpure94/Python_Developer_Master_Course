533f2b7d3eb44f7f7996b40168ee701cea7de7a4
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-09-05 17:08:09 +0530

    [ADD] Filter even numbers using filter()

---
+++ Part5/Phase4/filter_even_numbers.py
@@ -0,0 +1,6 @@
+"""Use filter() to find even numbers."""
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+even_numbers = list(filter(lambda x: x % 2 == 0, numbers))
+print(f"Even numbers: {even_numbers}")
