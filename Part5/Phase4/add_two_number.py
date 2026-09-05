9878e48730603c991558ed2aa94e01e26c1a6498
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-09-05 16:34:31 +0530

    [ADD] Add two numbers using lambda expression

---
+++ Part5/Phase4/add_two_number.py
@@ -0,0 +1,5 @@
+"""Lambda to add two numbers."""
+
+addition = lambda first_number, second_number: first_number + second_number
+result = addition(first_number=10, second_number=20)
+print(f"Addition: {result}")
