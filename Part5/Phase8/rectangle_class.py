37243d056e7bae98871563dc7b53ffa39512fbd8
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-09-08 20:42:43 +0530

    [ADD] Create a rectangle class and object

---
+++ Part5/Phase8/rectangle_class.py
@@ -0,0 +1,13 @@
+"""Create a Rectangle class."""
+
+
+class Rectangle:
+    """Represent a rectangle."""
+
+    def show(self) -> None:
+        """Display rectangle information."""
+        print("Rectangle class.")
+
+
+rectangle_obj = Rectangle()
+rectangle_obj.show()
