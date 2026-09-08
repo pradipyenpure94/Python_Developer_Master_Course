b01bbca70b2794363e77ab8d41ea29d7c2b4edcc
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-09-08 20:57:36 +0530

    [ADD] Create bank account class and object

---
+++ Part5/Phase8/bankaccount.py
@@ -0,0 +1,13 @@
+"""Create a BankAccount class."""
+
+
+class BankAccount:
+    """Represent a bank account."""
+
+    def show_info(self) -> None:
+        """Show bank account information."""
+        print("BankAccount class.")
+
+
+bank_account_obj = BankAccount()
+bank_account_obj.show_info()
