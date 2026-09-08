e78419e2ebcaeedaaa2145ae86885175a5b81023
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-09-08 20:28:32 +0530

    [ADD] Student class example

---
+++ Part5/Phase8/student_class.py
@@ -0,0 +1,13 @@
+"""Create a Student class."""
+
+
+class Student:
+    """Represent a student."""
+
+    def show(self) -> None:
+        """Display student information"""
+        print("Student class.")
+
+
+student_obj = Student()
+student_obj.show()
