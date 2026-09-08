52b4d099f2c77da369c97ae0ba4410029c3f391a
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-09-08 20:38:12 +0530

    [ADD]Create a product class

---
+++ Part5/Phase8/product_class.py
@@ -0,0 +1,13 @@
+"""Create a Product class."""
+
+
+class Product:
+    """Represent a product."""
+
+    def show(self) -> None:
+        """Display product information."""
+        print("Product class.")
+
+
+product_obj = Product()
+product_obj.show()
