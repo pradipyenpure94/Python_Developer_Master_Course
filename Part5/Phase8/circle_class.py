7e69a52ed259dd0277be8a07b554acf7c5b9881d
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-09-08 20:51:22 +0530

    [ADD] Create circle class and object

---
+++ Part5/Phase8/circle_class.py
@@ -0,0 +1,13 @@
+"""Create a Circle class."""
+
+
+class Circle:
+    """Represent a circle."""
+
+    def show_area(self) -> None:
+        """Show area of circle."""
+        print("Circle class.")
+
+
+circle_obj = Circle()
+circle_obj.show_area()
