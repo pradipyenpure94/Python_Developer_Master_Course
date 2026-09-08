0edad322e0a43b3ef4eae3dced65defb01fbfa45
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-09-08 20:33:37 +0530

    [ADD] Create an employee class

---
+++ Part5/Phase8/employee_class.py
@@ -0,0 +1,13 @@
+"""Create an Employee class."""
+
+
+class Employee:
+    """Represent an employee class."""
+
+    def show(self) -> None:
+        """Display employee information."""
+        print("Employee class.")
+
+
+employee_obj = Employee()
+employee_obj.show()
