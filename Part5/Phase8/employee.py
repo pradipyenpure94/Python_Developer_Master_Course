d944abb397dfa2d0e7a44a6c04bea62144664193
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-09-09 12:39:37 +0530

    [ADD] Implement the getter and setter methods

--- Part5/Phase8/employee.py
+++ Part5/Phase8/employee.py
@@ -1,4 +1,7 @@
-"""Create Employee with private salary."""
+"""
+1. Create Employee with private salary.
+2. Create getter and setter methods.
+"""
 
 
 class Employee:
@@ -6,3 +9,19 @@ class Employee:
 
     def __init__(self, salary: float) -> None:
         self.__salary = salary
+
+    def get_employee_salary(self) -> float:
+        """Return the employee salary."""
+        return self.__salary
+
+    def set_employee_salary(self, salary: float) -> None:
+        """Update employee salary."""
+        self.__salary = salary
+
+
+employee_obj = Employee(salary=5000)
+print(f"Current Employee Salary: {employee_obj.get_employee_salary()}")
+employee_obj.set_employee_salary(salary=4500)
+print(f"Current Employee Salary: {employee_obj.get_employee_salary()}")
+employee_obj.set_employee_salary(salary=9500)
+print(f"Current Employee Salary: {employee_obj.get_employee_salary()}")
