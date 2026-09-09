093ad6be20fa096b9eeec39be0ba3094da2abbaa
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-09-09 11:58:48 +0530

    [ADD] Check minimum account balance

--- Part5/Phase8/bank_account.py
+++ Part5/Phase8/bank_account.py
@@ -4,6 +4,8 @@
 3. Implement withdrawal operation.
 """
 
+MIN_ACCOUNT_BALANCE = 1000
+
 
 class BankAccount:
     """Represent a Bank Account."""
@@ -23,6 +25,8 @@ class BankAccount:
             raise ValueError("Withdraw amount must be greater than zero.")
         if self.__balance < amount:
             raise ValueError("Insufficient account balance.")
+        if self.__balance - MIN_ACCOUNT_BALANCE < amount:
+            raise ValueError("Minimum account balance must be maintained.")
         self.__balance -= amount
 
     def get_account_balance(self) -> float:
@@ -31,7 +35,7 @@ class BankAccount:
 
 
 bank_account_obj = BankAccount(balance=5000)
-bank_account_obj.deposit_amount(amount=500)
+bank_account_obj.deposit_amount(amount=1000)
 print(f"Current Balance: {bank_account_obj.get_account_balance()}")
-bank_account_obj.withdraw_amount(amount=400)
+bank_account_obj.withdraw_amount(amount=5900)
 print(f"Current Balance: {bank_account_obj.get_account_balance()}")
