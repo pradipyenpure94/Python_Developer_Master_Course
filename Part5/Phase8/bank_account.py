780f48efd8b53a8a844b444aea15d48139b9aba6
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-09-09 11:38:51 +0530

    [ADD] Implement deposit operation

--- Part5/Phase8/bank_account.py
+++ Part5/Phase8/bank_account.py
@@ -1,4 +1,7 @@
-"""Create BankAccount with private balance."""
+"""
+1. Create BankAccount with private balance.
+2. Implement deposit operation.
+"""
 
 
 class BankAccount:
@@ -7,5 +10,17 @@ class BankAccount:
     def __init__(self, balance: float) -> None:
         self.__balance = balance
 
+    def deposit_amount(self, amount: float) -> None:
+        """Deposit amount in Bank Account."""
+        if amount <= 0:
+            raise ValueError("Deposit amount must be greater than zero.")
+        self.__balance += amount
+
+    def get_account_balance(self) -> float:
+        """Return the account balance."""
+        return self.__balance
+
 
 bank_account_obj = BankAccount(balance=5000)
+bank_account_obj.deposit_amount(amount=500)
+print(f"Current Balance: {bank_account_obj.get_account_balance()}")
