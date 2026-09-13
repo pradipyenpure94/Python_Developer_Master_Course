73f7e570b66cf276a04c23ffb528cf5a97cdb981
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-09-13 18:33:17 +0530

    [ADD] Design and develop a custom squre number iterator

---
+++ Iterators_and_generators/squares_number.py
@@ -0,0 +1,30 @@
+"""Custom Iterator for Squares."""
+
+
+class SqureNumberIterator:
+    """Represent a squre number iterator."""
+
+    def __init__(self, limit: int) -> None:
+        self.current = 1
+        self.limit = limit
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.current <= self.limit:
+            value = self.current ** 2
+            self.current += 1
+            return value
+
+        raise StopIteration
+
+
+if __name__ == "__main__":
+    numbers = SqureNumberIterator(limit=5)
+
+    print(next(numbers))
+    print(next(numbers))
+    print("In for loop")
+    for number in numbers:
+        print(number)
