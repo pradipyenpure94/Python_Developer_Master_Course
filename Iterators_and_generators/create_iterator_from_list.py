83ac8c5aa5f86c561060d5f890f4a178716e6400
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-09-13 16:58:18 +0530

    [ADD] Create iterator from numbers list and iterate it

---
+++ Iterators_and_generators/create_iterator_from_list.py
@@ -0,0 +1,12 @@
+"""
+Create a list of numbers and manually iterate through
+it using iter() and next().
+"""
+
+numbers = [10, 20, 30, 40, 50]
+
+iterators = iter(numbers)
+
+print(next(iterators))
+print(next(iterators))
+print(next(iterators))
