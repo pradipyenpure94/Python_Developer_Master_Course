7537f63ae2f0e1990380477b0619176fc23444fb
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-09-13 15:16:34 +0530

    [ADD] Iterate over a string

---
+++ Iterators_and_generators/iterator_with_string.py
@@ -0,0 +1,11 @@
+"""Iterator with a string."""
+
+name = "Python"
+
+iterator = iter(name)
+
+print(next(iterator))
+print(next(iterator))
+print(next(iterator))
+print(next(iterator))
+print(next(iterator))
