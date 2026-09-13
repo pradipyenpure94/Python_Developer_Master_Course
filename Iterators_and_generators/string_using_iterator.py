ce1b59a3447950b7ec4dbf48c2ad268ec6d654d3
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-09-13 17:11:38 +0530

    [ADD] Iterate through string using iterator

---
+++ Iterators_and_generators/string_using_iterator.py
@@ -0,0 +1,12 @@
+"""Iterate through string using iterator."""
+
+name = "PYTHON"
+
+iterator = iter(name)
+
+print(next(iterator))
+print(next(iterator))
+print(next(iterator))
+print(next(iterator))
+print(next(iterator))
+print(next(iterator))
