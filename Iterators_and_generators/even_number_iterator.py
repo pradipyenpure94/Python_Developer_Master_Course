8e0ea0a7fd37cd8cfa4691b3b7deac1c693a6a80
pradipyenpure94 <pradipyenpure94@gmail.com>
2026-09-13 14:40:32 +0530

    [ADD] Design and develop even number iterator

---
+++ Iterators_and_generators/even_number_iterator.py
@@ -0,0 +1,30 @@
+"""Design our own custom Even iterator."""
+
+
+class EvenNumberIterator:
+    """Represent an even number iterator."""
+
+    def __init__(self, limit: int) -> None:
+        self.current = 2
+        self.limit = limit
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.current <= self.limit:
+            value = self.current
+            self.current += 2
+            return value
+
+        raise StopIteration
+
+
+if __name__ == "__main__":
+    try:
+        numbers = EvenNumberIterator(7)
+        print(next(numbers))
+        print(next(numbers))
+        print(next(numbers))
+    except StopIteration:
+        print("Error: StopIteration")
